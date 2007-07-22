591a105ae5b38194f778378403b43959ba18dfab
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-07-22 17:03:56 +0000

    date util tests

---
+++ main/test/com/gwtext/client/DateUtilTest.java
@@ -0,0 +1,43 @@
+/*
+ * GwtExt - Gwt Ext Integration library.
+ *
+ * Copyright (c) 2007, Sanjiv Jivan
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package com.gwtext.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+import com.gwtext.client.util.DateUtil;
+
+import java.util.Date;
+
+
+public class DateUtilTest extends GWTTestCase {
+
+    public String getModuleName() {
+        return "com.gwtext.Test";
+    }
+
+    public void testWeekOfYear() {
+        Date date = new Date(2007, 3, 12);
+        int woy = DateUtil.getWeekOfYear(date);
+
+        assertEquals(14, woy);
+    }
+
+
+}

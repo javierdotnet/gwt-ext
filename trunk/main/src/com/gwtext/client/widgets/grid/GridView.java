fac1288ec42d4d37c5193367c152b48ae8def31e
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-07-27 12:39:51 +0000

    added new API's to GridView

--- trunk/main/src/com/gwtext/client/widgets/grid/GridView.java
+++ trunk/main/src/com/gwtext/client/widgets/grid/GridView.java
@@ -21,10 +21,13 @@
 package com.gwtext.client.widgets.grid;
 
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.Element;
 import com.gwtext.client.core.ExtElement;
 import com.gwtext.client.core.JsObject;
+import com.gwtext.client.data.Record;
 
 public class GridView extends JsObject {
+
     public GridView(JavaScriptObject jsObj) {
         super(jsObj);
     }
@@ -47,7 +50,106 @@ public class GridView extends JsObject {
 
     public native void refresh() /*-{
         var view = this.@com.gwtext.client.core.JsObject::jsObj;
-        return view.refresh();
+        view.refresh();
     }-*/;
 
+    public native void refreshRow(int index) /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.refreshRow(index);
+    }-*/;
+
+    public native void refreshRow(Record record) /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        var recordJS = record.@com.gwtext.client.core.JsObject::jsObj;
+        view.refreshRow(recordJS);
+    }-*/;
+
+    public native void scrollToTop() /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.scrollToTop();
+    }-*/;
+
+    public native Element getHeaderCell(int index)/*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        var headerCellEl = view.getHeaderCell(index);
+        return headerCellEl === undefined ? null : headerCellEl;
+    }-*/;
+
+    public native Element getLockedRow(int index)/*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        var rowEl = view.getLockedRow(index);
+        return rowEl === undefined ? null : rowEl;
+    }-*/;
+
+    public native Element getRow(int index)/*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        var rowEl = view.getRow(index);
+        return rowEl === undefined ? null : rowEl;
+    }-*/;
+
+    public native Element getCell(int rowIndex, int colIndex)/*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        var el = view.getCell(rowIndex, colIndex);
+        return el=== undefined ? null : el;
+    }-*/;
+
+    public native void focusRow(int rowIndex) /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.focusRow(rowIndex);
+    }-*/;
+
+    public native void focusCell(int rowIndex, int colIndex, boolean hscroll) /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.focusCell(rowIndex, colIndex, hscroll);
+    }-*/;
+
+    public native void ensureVisible(int rowIndex, int colIndex, boolean hscroll) /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.ensureVisible(rowIndex, colIndex, hscroll);
+    }-*/;
+
+    public native void hideColumn(int colIndex) /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.hideColumn(colIndex);
+    }-*/;
+
+    public native void unhideColumn(int colIndex) /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.unhideColumn(colIndex);
+    }-*/;
+
+    public native void updateRows() /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.updateRows();
+    }-*/;
+
+    public native void stripeRows(int startRow) /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.stripeRows(startRow);
+    }-*/;
+
+    public native void autoSizeColumn(int colIndex, boolean forceMinSize) /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.autoSizeColumn(colIndex, forceMinSize);
+    }-*/;
+
+    public native void autoSizeColumn(int colIndex, boolean forceMinSize, boolean suppressEvent) /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.autoSizeColumn(colIndex, forceMinSize, suppressEvent);
+    }-*/;
+
+    public native void autoSizeColumns() /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.autoSizeColumns();
+    }-*/;
+
+    public native void fitColumns(boolean reserveScrollSpace) /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.fitColumns(reserveScrollSpace);
+    }-*/;
+
+    public native void updateHeaderSortState() /*-{
+        var view = this.@com.gwtext.client.core.JsObject::jsObj;
+        view.updateHeaderSortState();
+    }-*/;
 }

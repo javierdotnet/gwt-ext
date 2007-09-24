6b2e2dccbe09c27ac823f110120666a8d61ee87c
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-09-24 00:02:52 +0000

    added docs

--- main/src/com/gwtext/client/widgets/tree/event/DefaultSelectionModelListener.java
+++ main/src/com/gwtext/client/widgets/tree/event/DefaultSelectionModelListener.java
@@ -23,10 +23,30 @@ package com.gwtext.client.widgets.tree.event;
 import com.gwtext.client.widgets.tree.DefaultSelectionModel;
 import com.gwtext.client.widgets.tree.TreeNode;
 
+/**
+ * Listener for DefaultSelectionModel.
+ *
+ * @author Sanjiv Jivan
+ * @see DefaultSelectionModel
+ */
 public interface DefaultSelectionModelListener {
 
+    /**
+     * Fires before the selected node changes, return false to cancel the change.
+     * 
+     * @param sm selection model
+     * @param newNode the new selection
+     * @param oldNode the old selection
+     * @return false to cancel change
+     */
     boolean doBeforeSelect(DefaultSelectionModel sm, TreeNode newNode, TreeNode oldNode);
 
-    //note treenode is null when clearSelection is called, or when a node is unselected
+    /**
+     * Fires when the selected node changes. Note TreeNode passed is null when clearSelection is called,
+     * or when a node is unselected.
+     *
+     * @param sm selection model
+     * @param node new selection
+     */
     void onSelectionChange(DefaultSelectionModel sm, TreeNode node);
 }

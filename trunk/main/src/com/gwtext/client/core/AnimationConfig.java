dbd27605877f46933e1060d69168e5adeb54a0bc
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-09 03:20:53 +0000

    docs / typesafe constants

--- trunk/main/src/com/gwtext/client/core/AnimationConfig.java
+++ trunk/main/src/com/gwtext/client/core/AnimationConfig.java
@@ -19,18 +19,39 @@
  */
 package com.gwtext.client.core;
 
+import com.gwtext.client.animation.Easing;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 
+/**
+ * Configuration class for amination settings.
+ *  
+ * @see com.gwtext.client.core.BaseElement
+ */
 public class AnimationConfig extends BaseConfig {
 
+    /**
+     * The duration of the animation in seconds
+     *
+     * @param duration defaults to 0.35
+     */
     public void setDuration(float duration) {
         JavaScriptObjectHelper.setAttribute(jsObj, "duration", duration);
     }
 
-    public void setEasing(String easing) {
-        JavaScriptObjectHelper.setAttribute(jsObj, "easing", easing);
+    /**
+     * The Easing method.
+     *
+     * @param easing defaults to {@link com.gwtext.client.animation.Easing#EASE_NONE}
+     */
+    public void setEasing(Easing easing) {
+        JavaScriptObjectHelper.setAttribute(jsObj, "easing", easing.getMethod());
     }
 
+    /**
+     * A function to execute when the anim completes.
+     *
+     * @param callback the callback function
+     */
     public native void setCallback(Function callback) /*-{
         var config = this.@com.gwtext.client.core.JsObject::jsObj;
         config['callback'] = function() {

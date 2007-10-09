21685fa5829d60a7062bae094bf960a7b1aaa2bd
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-09 03:18:29 +0000

    docs / typesafe constants

--- main/src/com/gwtext/client/core/event/ConnectionListener.java
+++ main/src/com/gwtext/client/core/event/ConnectionListener.java
@@ -22,11 +22,35 @@ package com.gwtext.client.core.event;
 
 import com.gwtext.client.core.Connection;
 
+/**
+ * The ConnectionListener interface.
+ *
+ * @see Connection
+ */
 public interface ConnectionListener {
 
+    /**
+     * Fires before a network request is made to retrieve a data object.
+     *
+     * @param conn the Collection
+     * @return false to abort the request
+     */
     boolean doBeforeRequest(Connection conn);
 
+    /**
+     * Fires if the request was successfully completed.
+     *
+     * @param conn the Connection
+     * @param responseText the response text
+     */
     void onRequestComplete(Connection conn, String responseText);
 
+    /**
+     * Fires if an error HTTP status was returned from the server.
+     *
+     * @param conn the Connection
+     * @param httpStatus the http status code
+     * @param responseText the response text
+     */
     void onRequestException(Connection conn, int httpStatus, String responseText);
 }

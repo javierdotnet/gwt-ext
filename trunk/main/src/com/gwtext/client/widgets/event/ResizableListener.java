18e45eac07c5e858c31743b43bb05505d048ebe0
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-11 01:09:30 +0000

    added javadoc

--- trunk/main/src/com/gwtext/client/widgets/event/ResizableListener.java
+++ trunk/main/src/com/gwtext/client/widgets/event/ResizableListener.java
@@ -19,7 +19,26 @@
  */
 package com.gwtext.client.widgets.event;
 
+import com.gwtext.client.core.EventObject;
+import com.gwtext.client.widgets.Resizable;
+
 public interface ResizableListener {
 
-    void onResize();
+    /**
+     * Fires event before the resize.
+     *
+     * @param self this
+     * @param e the event object
+     * @return false to prevent the resize
+     */
+    boolean doBeforeResize(Resizable self, EventObject e);
+
+    /**
+     * Fires when the element is resized.
+     *
+     * @param self this
+     * @param width the new width
+     * @param height the new height
+     */
+    void onResize(Resizable self, int width, int height);
 }

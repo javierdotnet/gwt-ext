5e92093d5ac7d76892ec14a2a34ce9dbb8ed3352
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-11 02:03:50 +0000

    updates

--- trunk/main/src/com/gwtext/client/widgets/grid/event/EditorGridListener.java
+++ trunk/main/src/com/gwtext/client/widgets/grid/event/EditorGridListener.java
@@ -26,9 +26,43 @@ import com.gwtext.client.widgets.grid.Grid;
 //deails here http://extjs.com/forum/showthread.php?t=2834&highlight=validateedit
 public interface EditorGridListener {
 
+    /**
+     * Fires after a cell is edited.
+     *
+     * @param grid this
+     * @param record the record being edited
+     * @param field the field name being edited
+     * @param newValue the value being set
+     * @param oldValue the original value for the field, before the edit.
+     * @param rowIndex the grid row index
+     * @param colIndex the grid column index
+     */
     void onAfterEdit(Grid grid, Record record, String field, Object newValue, Object oldValue, int rowIndex, int colIndex);
 
+    /**
+     * Fires before cell editing is triggered.
+     *
+     * @param grid This grid
+     * @param record the record being edited
+     * @param field the field name being edited
+     * @param value the value for the field being edited
+     * @param rowIndex the grid row index
+     * @param colIndex the grid column index
+     * @return false to cancel the edit
+     */
     boolean doBeforeEdit(Grid grid, Record record, String field, Object value, int rowIndex, int colIndex);
 
+    /**
+     * Fires after a cell is edited, but before the value is set in the record. Return false to cancel the change.
+     *
+     * @param grid this
+     * @param record the record being edited
+     * @param field the field name being edited
+     * @param value the value being set
+     * @param originalValue the original value for the field, before the edit
+     * @param rowIndex the grid row index
+     * @param colIndex the grid column index
+     * @return false to cancel the change
+     */
     boolean doValidateEdit(Grid grid, Record record, String field, Object value, Object originalValue, int rowIndex, int colIndex);
 }

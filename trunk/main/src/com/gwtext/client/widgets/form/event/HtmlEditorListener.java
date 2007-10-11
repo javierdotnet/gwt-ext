e3c018e9aa6e6278ad63cea0bf25398222af3605
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-11 01:46:50 +0000

    added javadoc

--- trunk/main/src/com/gwtext/client/widgets/form/event/HtmlEditorListener.java
+++ trunk/main/src/com/gwtext/client/widgets/form/event/HtmlEditorListener.java
@@ -24,17 +24,59 @@ import com.gwtext.client.widgets.form.HtmlEditor;
 
 public interface HtmlEditorListener {
 
+    /**
+     * Fires when the editor is first receives the focus. Any insertion must wait until after this event.
+     * 
+     * @param field this
+     */
     void onActivate(HtmlEditor field);
 
+    /**
+     * Fires before the iframe editor is updated with content from the textarea. Return false to cancel the push.
+     *
+     * @param field this
+     * @param html the html
+     * @return false to cancel
+     */
     boolean doBeforePush(HtmlEditor field, String html);
 
+    /**
+     * Fires before the textarea is updated with content from the editor iframe. Return false to cancel the sync.
+     *
+     * @param field this
+     * @param html the html
+     * @return false to cancel sync
+     */
     boolean doBeforeSync(HtmlEditor field, String html);
 
+    /**
+     * Fires when the editor switches edit modes.
+     *
+     * @param field this
+     * @param sourceEdit true if source edit, false if standard editing
+     */
     void onEditModeChange(HtmlEditor field, boolean sourceEdit);
 
+    /**
+     * Fires when the editor is fully initialized (including the iframe)
+     *
+     * @param field this
+     */
     void onInitialize(HtmlEditor field);
 
+    /**
+     * Fires when the iframe editor is updated with content from the textarea.
+     *
+     * @param field this
+     * @param html the html
+     */
     void onPush(HtmlEditor field, String html);
 
+    /**
+     * Fires when the textarea is updated with content from the editor iframe.
+     *
+     * @param field this
+     * @param html the html
+     */
     void onSync(HtmlEditor field, String html);
 }

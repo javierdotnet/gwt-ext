5a622ce61f2e85f2b9c4c3dcf1ce4e054cc1f67f
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-11 01:57:31 +0000

    added javadoc

--- trunk/main/src/com/gwtext/client/widgets/grid/event/ColumnModelListener.java
+++ trunk/main/src/com/gwtext/client/widgets/grid/event/ColumnModelListener.java
@@ -24,13 +24,48 @@ import com.gwtext.client.widgets.grid.ColumnModel;
 
 public interface ColumnModelListener {
 
+    /**
+     * Fires when a column's locked state is changed.
+     *
+     * @param cm this
+     * @param colIndex the column index
+     * @param locked true if locked
+     */
     void onColumnLockChange(ColumnModel cm, int colIndex, boolean locked);
 
+    /**
+     * Fires when a column is moved.
+     *
+     * @param cm this
+     * @param oldIndex the old column index
+     * @param newIndex the new column index
+     */
     void onColumnMoved(ColumnModel cm, int oldIndex, int newIndex);
 
-    void onHeaderChange(ColumnModel cm, int oldIndex, String newText);
+    /**
+     * Fires when the text of a header changes.
+     *
+     * @param cm this
+     * @param columnIndex the column index
+     * @param newText the new header text
+     */
+    void onHeaderChange(ColumnModel cm, int columnIndex, String newText);
 
+    /**
+     * Fires when a column is hidden or "unhidden".
+     * 
+     * @param cm this
+     * @param colIndex the column index
+     * @param hidden true if hidden, false otherwise
+     */
     void onHiddenChange(ColumnModel cm, int colIndex, boolean hidden);
 
+    /**
+     * Fires when the width of a column changes.
+     *
+     * @param cm this
+     * @param colIndex the column index
+     * @param newWidth the new width
+     */
     void onWidthChange(ColumnModel cm, int colIndex, int newWidth);
 }

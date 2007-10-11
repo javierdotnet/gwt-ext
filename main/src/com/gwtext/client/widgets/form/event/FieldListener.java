3b3a186d989e34f843cd5d1e5fe7c5a3fdc337ea
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-11 01:40:42 +0000

    added javadoc

--- main/src/com/gwtext/client/widgets/form/event/FieldListener.java
+++ main/src/com/gwtext/client/widgets/form/event/FieldListener.java
@@ -25,15 +25,50 @@ import com.gwtext.client.widgets.form.Field;
 
 public interface FieldListener {
 
+    /**
+     * Fires when this field loses input focus.
+     *
+     * @param field this
+     */
     void onBlur(Field field);
 
+    /**
+     * Fires just before the field blurs if the field value has changed.
+     *
+     * @param field this
+     * @param newVal the new value
+     * @param oldVal the old value
+     */
     void onChange(Field field, Object newVal, Object oldVal);
 
+    /**
+     * Fires when this field receives input focus.
+     *
+     * @param field this
+     */
     void onFocus(Field field);
 
+    /**
+     * Fires after the field has been marked as invalid.
+     *
+     * @param field this
+     * @param msg the validation message
+     */
     void onInvalid(Field field, String msg);
 
+    /**
+     * Fires when any key related to navigation (arrows, tab, enter, esc, etc.) is pressed. You can check
+     * {@link EventObject#getKey} to determine which key was pressed.
+     *
+     * @param field this
+     * @param e the event object
+     */
     void onSpecialKey(Field field, EventObject e);
 
+    /**
+     * Fires after the field has been validated with no errors.
+     * 
+     * @param field this
+     */
     void onValid(Field field);
 }

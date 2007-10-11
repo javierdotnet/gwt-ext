e7314a7c93d4fb706f3a8c53e1e3a25bc0eb4388
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-11 01:53:32 +0000

    added javadoc

--- trunk/main/src/com/gwtext/client/widgets/grid/event/CellSelectionModelListener.java
+++ trunk/main/src/com/gwtext/client/widgets/grid/event/CellSelectionModelListener.java
@@ -25,9 +25,31 @@ import com.gwtext.client.widgets.grid.CellSelectionModel;
 
 public interface CellSelectionModelListener {
 
+    /**
+     * Fires before a cell is selected.
+     *
+     * @param sm this
+     * @param rowIndex The selected row index
+     * @param colIndex The selected cell index
+     * @return false to cancel the row select
+     */
     boolean doBeforeRowSelect(CellSelectionModel sm, int rowIndex, int colIndex);
 
+    /**
+     * Fires when a cell is selected.
+     *
+     * @param sm this
+     * @param rowIndex The selected row index
+     * @param colIndex The selected cell index
+     */
     void onCellSelect(CellSelectionModel sm, int rowIndex, int colIndex);
 
+    /**
+     * Fires when the active selection changes.
+     *
+     * @param sm this
+     * @param record the selected record. null for no selection
+     * @param rowIndexColIndex array of rowIndex, columnIndex
+     */
     void onSelectionChange(CellSelectionModel sm, Record record, int[] rowIndexColIndex);
 }

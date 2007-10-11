cc3875038a8b03393a3052f104355c66a3ddf9ee
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-11 02:23:35 +0000

    added javadoc

--- trunk/main/src/com/gwtext/client/widgets/grid/event/GridMouseListener.java
+++ trunk/main/src/com/gwtext/client/widgets/grid/event/GridMouseListener.java
@@ -24,11 +24,31 @@ import com.gwtext.client.core.EventObject;
 
 public interface GridMouseListener {
 
+    /**
+     * The raw mousedown event for the entire grid.
+     *
+     * @param e the event object
+     */
     void onMouseDown(EventObject e);
 
+    /**
+     * The raw mouseout event for the entire grid.
+     *
+     * @param e the event object
+     */
     void onMouseOut(EventObject e);
 
+    /**
+     * The raw mouseover event for the entire grid.
+     *
+     * @param e the event object
+     */
     void onMouseOver(EventObject e);
 
+    /**
+     * The raw mouseup event for the entire grid.
+     *
+     * @param e the event object
+     */
     void onMouseUp(EventObject e);
 }

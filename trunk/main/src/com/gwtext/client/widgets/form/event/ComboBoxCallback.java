1f64dcb8d972f747d51981b921759c33a4149d03
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-11 01:36:56 +0000

    added javadoc

--- trunk/main/src/com/gwtext/client/widgets/form/event/ComboBoxCallback.java
+++ trunk/main/src/com/gwtext/client/widgets/form/event/ComboBoxCallback.java
@@ -24,28 +24,51 @@ import com.google.gwt.core.client.JavaScriptObject;
 import com.gwtext.client.core.JsObject;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 
+/**
+ * @see ComboBoxListener#doBeforeQuery(com.gwtext.client.widgets.form.ComboBox, ComboBoxCallback)
+ */
 public class ComboBoxCallback extends JsObject {
 
-    public ComboBoxCallback(JavaScriptObject jsObj) {
+    private ComboBoxCallback(JavaScriptObject jsObj) {
         super(jsObj);
     }
 
-    public static ComboBoxCallback instance(JavaScriptObject jsObj) {
+    private static ComboBoxCallback instance(JavaScriptObject jsObj) {
         return new ComboBoxCallback(jsObj);
     }
 
+    /**
+     * Returns the query.
+     *
+     * @return the query
+     */
     public String getQuery() {
         return JavaScriptObjectHelper.getAttribute(jsObj, "query");
     }
 
+    /**
+     * Sets the query.
+     *
+     * @param query the query
+     */
     public void setQuery(String query) {
         JavaScriptObjectHelper.setAttribute(jsObj, "query", query);
     }
 
+    /**
+     * Set true to force "all" query.
+     *
+     * @param forceAll true to force "all" query
+     */
     public void setForceAll(boolean forceAll) {
         JavaScriptObjectHelper.setAttribute(jsObj, "forceAll", forceAll);
     }
 
+    /**
+     * Returns true if query is force "all".
+     *
+     * @return true if force "all"
+     */
     public boolean getForceAll() {
         return JavaScriptObjectHelper.getAttributeAsBoolean(jsObj, "forceAll");
     }

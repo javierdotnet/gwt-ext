1bee510658498ad22900782fe45f55d25f40dbeb
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-12 02:25:00 +0000

    added javadoc

--- trunk/main/src/com/gwtext/client/widgets/tree/event/TreeNodeListener.java
+++ trunk/main/src/com/gwtext/client/widgets/tree/event/TreeNodeListener.java
@@ -25,27 +25,104 @@ import com.gwtext.client.data.Node;
 
 public interface TreeNodeListener {
 
+    /**
+     * Fires right before the child nodes for this node are rendered.
+     *
+     * @param node the node
+     * @return false to cancel
+     */
     boolean doBeforeChildrenRendered(Node node);
 
+    /**
+     * Fires before click processing. Return false to cancel the default action.
+     *
+     * @param node the node
+     * @param e the event object
+     * @return false to cancel
+     */
     boolean doBeforeClick(Node node, EventObject e);
 
+    /**
+     * Fires before this node is collapsed, return false to cancel.
+     *
+     * @param node the node
+     * @param deep whether deep collapse
+     * @param anim whether animated
+     * @return false to cancel
+     */
     boolean doBeforeCollapse(Node node, boolean deep, boolean anim);
 
+    /**
+     * Fires before this node is expanded, return false to cancel.
+     *
+     * @param node the node
+     * @param deep whether deep expand
+     * @param anim whether animated
+     * @return false to cancel
+     */
     boolean doBeforeExpand(Node node, boolean deep, boolean anim);
 
+    /**
+     * Fires when a node with a checkbox's checked property changes.
+     *
+     * @param node the node
+     * @param checked the new checked value
+     * @return false to cancel
+     */
     boolean doBeforeCheckChanged(Node node, boolean checked);
 
+    /**
+     * Fires when this node is clicked.
+     *
+     * @param node the node
+     * @param e the event object
+     */
     void onClick(Node node, EventObject e);
 
+    /**
+     * Fires when this node is collapsed.
+     *
+     * @param node the node
+     */
     void onCollapse(Node node);
 
+    /**
+     * Fires when this node is right clicked.
+     *
+     * @param node the node
+     * @param e the event obejct
+     */
     void onContextMenu(Node node, EventObject e);
 
+    /**
+     * Fires when this node is double clicked.
+     *
+     * @param node the node
+     * @param e the event object
+     */
     void onDblClick(Node node, EventObject e);
 
+    /**
+     * Fires when the disabled status of this node changes.
+     *
+     * @param node the node
+     * @param disabled true if disabled
+     */
     void onDisabledChange(Node node, boolean disabled);
 
+    /**
+     * Fires when this node is expanded.
+     *
+     * @param node the node
+     */
     void onExpand(Node node);
 
+    /**
+     * Fires when the text for this node is changed.
+     *
+     * @param node the node
+     * @param text the new text
+     * @param oldText the old text
+     */
     void onTextChange(Node node, String text, String oldText);
 }

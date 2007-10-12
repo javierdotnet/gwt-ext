5e0ef71d85a67f45a30c40a4e3d5733eb6bf4a45
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-12 02:15:27 +0000

    added javadoc

--- trunk/main/src/com/gwtext/client/widgets/menu/event/MenuListener.java
+++ trunk/main/src/com/gwtext/client/widgets/menu/event/MenuListener.java
@@ -26,21 +26,70 @@ import com.gwtext.client.widgets.menu.Menu;
 
 public interface MenuListener {
 
+    /**
+     * Fires before this menu is hidden.
+     *
+     * @param menu this
+     * @return false to cancel the hide
+     */
     boolean doBeforeHide(Menu menu);
 
+    /**
+     * Fires before this menu is displayed.
+     *
+     * @param menu this
+     * @return false to cancel the show
+     */
     boolean doBeforeShow(Menu menu);
 
-    //todo inconsistent order of parms sent my ext
+    //todo inconsistent order of parms sent my ext, docs say menu item sent, but only id sent
+    /**
+     * Fires when this menu is clicked (or when the enter key is pressed while it is active).
+     * 
+     * @param menu this
+     * @param menuItemId the menu item ID
+     * @param e the event object
+     */
     void onClick(Menu menu, String menuItemId, EventObject e);
 
+    /**
+     * Fires after this menu is hidden.
+     *
+     * @param menu this
+     */
     void onHide(Menu menu);
 
     //todo ext inconsistent here
+    /**
+     * Fires when a menu item contained in this menu is clicked.
+     * 
+     * @param item the clicked item
+     * @param e the event object
+     */
     void onItemClick(BaseItem item, EventObject e);
 
+    /**
+     * Fires when the mouse exits this menu.
+     *
+     * @param menu this
+     * @param menuItem the menu item
+     * @param e the event object
+     */
     void onMouseOut(Menu menu, BaseItem menuItem, EventObject e);
 
+    /**
+     * Fires when the mouse is hovering over this menu.
+     *
+     * @param menu this
+     * @param menuItem the menu item
+     * @param e the event object
+     */
     void onMouseOver(Menu menu, BaseItem menuItem, EventObject e);
 
+    /**
+     * Fires after this menu is displayed.
+     *
+     * @param menu this
+     */
     void onShow(Menu menu);
 }

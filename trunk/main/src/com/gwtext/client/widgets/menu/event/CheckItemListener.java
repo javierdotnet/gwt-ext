15339b3d56d2fc9d05aac057ece23847f4fa165e
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-12 02:07:30 +0000

    added javadoc

--- trunk/main/src/com/gwtext/client/widgets/menu/event/CheckItemListener.java
+++ trunk/main/src/com/gwtext/client/widgets/menu/event/CheckItemListener.java
@@ -24,7 +24,20 @@ import com.gwtext.client.widgets.menu.CheckItem;
 
 public interface CheckItemListener {
 
+    /**
+     * Fires before the checked value is set, providing an opportunity to cancel if needed.
+     *
+     * @param item this
+     * @param checked the new checked value that will be set
+     * @return false to cancel
+     */
     boolean doBeforeCheckChange(CheckItem item, boolean checked);
 
+    /**
+     * Fires after the checked value has been set.
+     *
+     * @param item this
+     * @param checked the checked value that was set
+     */
     void onCheckChange(CheckItem item, boolean checked);
 }

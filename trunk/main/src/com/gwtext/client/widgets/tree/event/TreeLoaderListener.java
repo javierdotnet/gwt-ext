21e1bcc215e7a12ff6beea6b19ec3a4f0a3a62d5
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-12 02:19:54 +0000

    added javadoc

--- trunk/main/src/com/gwtext/client/widgets/tree/event/TreeLoaderListener.java
+++ trunk/main/src/com/gwtext/client/widgets/tree/event/TreeLoaderListener.java
@@ -25,9 +25,30 @@ import com.gwtext.client.widgets.tree.TreeNode;
 
 public interface TreeLoaderListener {
 
+    /**
+     * Fires before a network request is made to retrieve the Json text which specifies a node's children.
+     * 
+     * @param self this
+     * @param node the node
+     * @return false to cancel
+     */
     boolean doBeforeLoad(TreeLoader self, TreeNode node);
 
+    /**
+     * Fires when the node has been successfuly loaded.
+     * 
+     * @param self this
+     * @param node the node
+     * @param response  the response text containing the data from the server
+     */
     void onLoad(TreeLoader self, TreeNode node, String response);
 
+    /**
+     * Fires if the network request failed.
+     * 
+     * @param self this
+     * @param node the node
+     * @param response the response  containing the data from the server
+     */
     void onLoadException(TreeLoader self, TreeNode node, String response);
 }

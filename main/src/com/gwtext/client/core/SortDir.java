b35b3887c7fe15121fa5d3e1407a8296bb059b24
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-16 04:54:03 +0000

    new class

---
+++ main/src/com/gwtext/client/core/SortDir.java
@@ -0,0 +1,39 @@
+/*
+ * GwtExt - Gwt Ext Integration library.
+ *
+ * Copyright (c) 2007, Sanjiv Jivan
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package com.gwtext.client.core;
+
+/**
+ * Constants for Sort direction.
+ */
+public class SortDir {
+
+    public static SortDir ASC = new SortDir("ASC");
+    public static SortDir DESC = new SortDir("DESC");
+
+    private String direction;
+
+    private SortDir(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}

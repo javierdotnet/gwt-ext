9b7ca68f1097c6a4569948a83e325c53bfe7a1fc
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-16 03:19:03 +0000

    added javadocs

--- trunk/main/src/com/gwtext/client/widgets/form/HtmlEditor.java
+++ trunk/main/src/com/gwtext/client/widgets/form/HtmlEditor.java
@@ -24,12 +24,31 @@ import com.google.gwt.core.client.JavaScriptObject;
 import com.gwtext.client.widgets.Toolbar;
 import com.gwtext.client.widgets.form.event.HtmlEditorListener;
 
+/**
+ * Provides a lightweight HTML Editor component.
+ *
+ * Note: The focus/blur and validation marking functionality inherited from {@link Field} is NOT supported by this editor.
+ * <br/><br/>
+ * An Editor is a sensitive component that can't be used in all spots standard fields can be used. Putting an Editor within any element that has display set to 'none' can cause problems in Safari and Firefox.
+ * <br/> <br/>
+ *
+ * There can only be one HtmlEditor on a page at a time.
+ */
 public class HtmlEditor extends Field {
 
+    /**
+     * Construct a new HtmlEditor.
+     *
+     */
     public HtmlEditor() {
         setJsObj(create(null));
     }
 
+    /**
+     * Construct a new HtmlEditor using the specified configuration.
+     *
+     * @param config the configuration object
+     */
     public HtmlEditor(HtmlEditorConfig config) {
         super(config);
         if (config.getHtmlEditorListener() != null) {
@@ -41,16 +60,22 @@ public class HtmlEditor extends Field {
         return new $wnd.Ext.form.HtmlEditor(jsObj);
     }-*/;
 
-    public native void cleanHtml(String html) /*-{
-        var he = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
-        he.cleanHtml(html);
-    }-*/;
-
+    /**
+     * Executes a Midas editor command directly on the editor document. For visual commands, you should use relayCmd instead. This should only be called after the editor is initialized.
+     *
+     * @param cmd the Midas command
+     * @param value the value to pass to the command
+     */
     public native void execCmd(String cmd, String value) /*-{
         var he = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         he.execCmd(cmd, value);
     }-*/;
 
+    /**
+     * Returns the editor's toolbar. This is only available after the editor has been rendered.
+     *
+     * @return toolbar
+     */
     public Toolbar getToolbar() {
         return new Toolbar(getToolbar(jsObj));
     }
@@ -59,21 +84,42 @@ public class HtmlEditor extends Field {
         return he.getToolbar();    
     }-*/;
 
+    /**
+     * Inserts the passed text at the current cursor position. Note: the editor must be initialized and activated to insert text.
+     * 
+     * @param text the text to insert
+     */
     public native void insertAtCursor(String text) /*-{
         var he = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         he.insertAtCursor(text);
     }-*/;
 
+    /**
+     * Executes a Midas editor command on the editor document and performs necessary focus and toolbar updates. This should only be called after the editor is initialized.
+     * 
+     * @param cmd the Midas command
+     * @param value the value to pass to the command
+     */
     public native void relayCmd(String cmd, String value) /*-{
         var he = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         he.relayCmd(cmd, value);
     }-*/;
 
+    /**
+     * Toggles the editor between standard and source edit mode.
+     * 
+     * @param sourceEdit true for source edit, false for standard
+     */
     public native void toggleSourceEdit(boolean sourceEdit) /*-{
         var he = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         he.toggleSourceEdit(sourceEdit);
     }-*/;
 
+    /**
+     * Adds a HtmlEditor listener.
+     *
+     * @param listener the listener
+     */
     public native void addHtmlEditorListener(HtmlEditorListener listener) /*-{
         var fieldJ = this;
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;

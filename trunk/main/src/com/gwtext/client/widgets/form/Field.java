e1a538df4aceaa6fa5745c895a2b5046724f68b3
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-16 01:42:15 +0000

    added javadocs

--- trunk/main/src/com/gwtext/client/widgets/form/Field.java
+++ trunk/main/src/com/gwtext/client/widgets/form/Field.java
@@ -26,12 +26,23 @@ import com.google.gwt.user.client.Element;
 import com.gwtext.client.widgets.BoxComponent;
 import com.gwtext.client.widgets.form.event.FieldListener;
 
+/**
+ * Base class for form fields that provides default event handling, sizing, value handling and other functionality.
+ */
 public class Field extends BoxComponent {
 
+    /**
+     * Creates a new Field.
+     */
     public Field() {
         setJsObj(create(null));
     }
 
+    /**
+     * Creates a new Field using the specified configuration.
+     *
+     * @param config field config
+     */
     public Field(FieldConfig config) {
         setJsObj(create(config.getJsObj()));        
         if (config.getFieldListener() != null) {
@@ -60,6 +71,11 @@ public class Field extends BoxComponent {
         throw new IllegalArgumentException("must be overridden");
     }
 
+    /**
+     * Apply the behaviors of this component to an existing element. This is used instead of render().
+     *
+     * @param id the ID of the node
+     */
     public void applyTo(String id) {
         Element el = DOM.getElementById(id);
         if (el == null) {
@@ -69,6 +85,11 @@ public class Field extends BoxComponent {
         applyTo(jsObj, id);
     }
 
+    /**
+     * Apply the behaviors of this component to an existing element. This is used instead of render().
+     *
+     * @param element the element
+     */
     public native void applyTo(Element element) /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         field.applyTo(element);
@@ -78,57 +99,109 @@ public class Field extends BoxComponent {
         field.applyTo(id);
     }-*/;
 
+    /**
+     * Clear any invalid styles/messages for this field.
+     */
     public native void clearInvalid() /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         field.clearInvalid();
     }-*/;
 
+    /**
+     * Returns the name attribute of the field if available.
+     *
+     * @return  the name attribute of the Field
+     */
     public native String getName() /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         return field.getName();
     }-*/;
 
+    /**
+     * Returns the raw data value which may or may not be a valid, defined value.
+     *
+     * @return the raw field value
+     */
     public native String getRawValue() /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         return field.getRawValue();
     }-*/;
 
+    /**
+     * Return the value of the field as a String.
+     *
+     * @return value as String
+     */
     public native String getValueAsString() /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         var value = field.getValue();
         return (value == null || value === undefined ) ? '' : value.toString();
     }-*/;
 
+    /**
+     * Returns true if this field has been changed since it was originally loaded and is not disabled.
+     *
+     * @return true if field changed
+     */
     public native boolean isDirty() /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         return field.isDirty();
     }-*/;
 
+    /**
+     * Returns whether or not the field value is currently valid.
+     *
+     * @return true if valid
+     */
     public native boolean isValid() /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         return field.isValid();
     }-*/;
 
+    /**
+     * Returns whether or not the field value is currently valid.
+     * 
+     * @param preventMark true to disable marking the field invalid
+     * @return true if valid
+     */
     public native boolean isValid(boolean preventMark) /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         return field.isValid(preventMark);
     }-*/;
 
+    /**
+     * Mark this field as invalid.
+     *
+     * @param message the validation message
+     */
     public native void markInvalid(String message) /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         field.markInvalid(message);
     }-*/;
 
+    /**
+     * Resets the current field value to the originally loaded value and clears any validation messages.
+     */
     public native void reset() /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         field.reset();
     }-*/;
 
+    /**
+     * Sets the underlying DOM field's value directly, bypassing validation. To set the value with validation use {@link #setValue(String)}.
+     *
+     * @param value field value
+     */
     public native void setRawValue(String value) /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         field.setRawValue(value);
     }-*/;
 
+    /**
+     * Sets a data value into the field and validates it. To set the value directly without validation see {@link #setRawValue(String)}.
+     *
+     * @param value the field value
+     */
     public native void setValue(String value) /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         field.setValue(value);
@@ -139,6 +212,11 @@ public class Field extends BoxComponent {
         return field.validate();
     }-*/;
 
+    /**
+     * Add a Field Listener.
+     *
+     * @param listener the listener
+     */
     public native void addFieldListener(FieldListener listener) /*-{
         var fieldJ = this;
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;

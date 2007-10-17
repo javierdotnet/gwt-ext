116f1c709652ac336d356a313313007966074d8e
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-17 04:50:44 +0000

    added javadocs

--- trunk/main/src/com/gwtext/client/widgets/grid/Renderer.java
+++ trunk/main/src/com/gwtext/client/widgets/grid/Renderer.java
@@ -23,7 +23,24 @@ package com.gwtext.client.widgets.grid;
 import com.gwtext.client.data.Record;
 import com.gwtext.client.data.Store;
 
+/**
+ * Inteface for custom Grid column rendering.
+ *
+ * @see com.gwtext.client.widgets.grid.ColumnConfig#setRenderer(Renderer)
+ */
 public interface Renderer {
 
+    /**
+     * Renderer function.
+     *
+     * @param value the cells value. When working with numberic data, cast the value to {@link java.lang.Number}
+     * and not to a specific data type line Integer or Float as the class type is inferred from the data
+     * @param cellMetadata the grid cell metadata. Can be use to customize the apperanace of the grid cell
+     * @param record the record corresponding to the row
+     * @param rowIndex the row index
+     * @param colNum the column number
+     * @param store the Grid's data store
+     * @return the rendered cell content (can include html)
+     */
     String render(Object value, CellMetadata cellMetadata, Record record, int rowIndex, int colNum, Store store);
 }

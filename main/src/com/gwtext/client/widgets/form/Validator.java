93e11851305c8854ada6c160feb664773a75a91f
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-17 02:35:45 +0000

    added javadocs

--- main/src/com/gwtext/client/widgets/form/Validator.java
+++ main/src/com/gwtext/client/widgets/form/Validator.java
@@ -20,7 +20,23 @@
 
 package com.gwtext.client.widgets.form;
 
+/**
+ * Interface that must be implemented for custom Field validation logic.
+ *
+ * @see com.gwtext.client.widgets.form.TextFieldConfig#setValidator(Validator)
+ */
 public interface Validator {
 
+    /**
+     *
+     * Return true if Field value is valid and false if invalid. The value set in {@link FieldConfig#setInvalidText(String)}
+     * will be used as the error message. For a custom error message throw a ValidationException with the
+     * error message. The error message of the ValidationException will be dispalyed.
+     *
+     *
+     * @param value the field value to test
+     * @return true if valid, false if invalid.
+     * @throws ValidationException when validate fails
+     */
     boolean validate(String value) throws ValidationException;
 }

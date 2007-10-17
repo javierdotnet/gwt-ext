437aea5236487d243f0aad145ba38cf4c8d8c502
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-17 02:52:47 +0000

    added javadocs

--- main/src/com/gwtext/client/widgets/form/TextFieldConfig.java
+++ main/src/com/gwtext/client/widgets/form/TextFieldConfig.java
@@ -24,42 +24,93 @@ import com.google.gwt.core.client.JavaScriptObject;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 import com.gwtext.client.widgets.form.event.TextFieldListener;
 
+/**
+ * {@link TextField} configuration class.
+ * 
+ */
 public class TextFieldConfig extends FieldConfig {
 
     private TextFieldListener textFieldListener;
 
+    /**
+     * False to validate that the value length > 0 (defaults to true).
+     *
+     * @param allowBlank false to disallow blank
+     */
     public void setAllowBlank(boolean allowBlank) {
         JavaScriptObjectHelper.setAttribute(jsObj, "allowBlank", allowBlank);
     }
 
+    /**
+     * Error text to display if the allow blank validation fails (defaults to "This field is required").
+     *
+     * @param blankText error message for blank field
+     */
     public void setBlankText(String blankText) {
         JavaScriptObjectHelper.setAttribute(jsObj, "blankText", blankText);
     }
 
+    /**
+     * True to disable input keystroke filtering (defaults to false).
+     *
+     * @param disableKeyFilter true to disable 
+     */
     public void setDisableKeyFilter(boolean disableKeyFilter) {
         JavaScriptObjectHelper.setAttribute(jsObj, "disableKeyFilter", disableKeyFilter);
     }
 
+    /**
+     * The CSS class to apply to an empty field to style the emptyText (defaults to 'x-form-empty-field'). This class is
+     * automatically added and removed as needed depending on the current field value.
+     *
+     * @param emptyClass the empty field CSS class
+     */
     public void setEmptyClass(String emptyClass) {
         JavaScriptObjectHelper.setAttribute(jsObj, "emptyClass", emptyClass);
     }
 
+    /**
+     * The default text to display in an empty field (defaults to null).
+     *
+     * @param emptyText the empty field text
+     */
     public void setEmptyText(String emptyText) {
         JavaScriptObjectHelper.setAttribute(jsObj, "emptyText", emptyText);
     }
 
+    /**
+     * True if this field should automatically grow and shrink to its content.
+     * 
+     * @param grow true to allow grow
+     */
     public void setGrow(boolean grow) {
         JavaScriptObjectHelper.setAttribute(jsObj, "grow", grow);
     }
 
+    /**
+     *  The maximum width to allow when grow = true (defaults to 800)
+     *
+     * @param growMax the max width
+     */
     public void setGrowMax(int growMax) {
         JavaScriptObjectHelper.setAttribute(jsObj, "growMax", growMax);
     }
 
+    /**
+     * The minimum width to allow when grow = true (defaults to 30).
+     * 
+     * @param growMin the minimum width
+     */
     public void setGrowMin(int growMin) {
         JavaScriptObjectHelper.setAttribute(jsObj, "growMin", growMin);
     }
 
+    /**
+     * An input mask regular expression that will be used to filter keystrokes that
+     * don't match (defaults to null).
+     *
+     * @param maskRe the mask regular expression
+     */
     public native void setMaskRe(String maskRe) /*-{
         var jsObj = this.@com.gwtext.client.core.JsObject::jsObj;
         jsObj['maskRe'] = new $wnd.RegExp(maskRe);
@@ -69,22 +120,51 @@ public class TextFieldConfig extends FieldConfig {
         JavaScriptObjectHelper.setAttribute(jsObj, "maxLength", maxLength);
     }
 
+    /**
+     * Error text to display if the maximum length validation fails.
+     * (defaults to "The maximum length for this field is {maxLength}")
+     *
+     * @param maxLengthText the max lenght error text
+     */
     public void setMaxLengthText(String maxLengthText) {
         JavaScriptObjectHelper.setAttribute(jsObj, "maxLengthText", maxLengthText);
     }
 
+    /**
+     * Minimum input field length required (defaults to 0).
+     *
+     * @param minLength the min length
+     */
     public void setMinLength(int minLength) {
         JavaScriptObjectHelper.setAttribute(jsObj, "minLength", minLength);
     }
 
+    /**
+     * Error text to display if the minimum length validation fails.
+     *  (defaults to "The minimum length for this field is {minLength}")
+     *
+     * @param minLengthText the min length error text
+     */
     public void setMinLengthText(String minLengthText) {
         JavaScriptObjectHelper.setAttribute(jsObj, "minLengthText", minLengthText);
     }
 
+    /**
+     * Set true if field is a password field.
+     *
+     * @param password true if passowrd field
+     */
     public void setPassword(boolean password) {
         if(password) setInputType("password");
     }
-            
+
+    /**
+     * A Regular Expressionto be tested against the field value during validation (defaults to null).
+     * If available, this regex will be evaluated only after the basic validators all return true, and will be passed
+     * the current field value. If the test fails, the field will be marked invalid using regexText.
+     *
+     * @param regex the regular expression
+     */
     public void setRegex(String regex) {
         setRegex(jsObj, regex);
     }
@@ -93,14 +173,29 @@ public class TextFieldConfig extends FieldConfig {
         config['regex'] = new $wnd.RegExp(regex);
     }-*/;
 
+    /**
+     * The error text to display if regex is used and the test fails during validation (defaults to "").
+     *
+     * @param regexText the regexp text
+     */
     public void setRegexText(String regexText) {
         JavaScriptObjectHelper.setAttribute(jsObj, "regexText", regexText);
     }
 
+    /**
+     * True to automatically select any existing field text when the field receives input focus (defaults to false).
+     *
+     * @param selectOnFocus true to select text on focus
+     */
     public void setSelectOnFocus(boolean selectOnFocus) {
         JavaScriptObjectHelper.setAttribute(jsObj, "selectOnFocus", selectOnFocus);
     }
 
+    /**
+     * Set a custom Validator for the Field.
+     *
+     * @param validator the field validator
+     */
     public void setValidator(Validator validator) {
         setValidator(jsObj, validator);
     }
@@ -127,10 +222,20 @@ public class TextFieldConfig extends FieldConfig {
     }-*/;
 
     //http://extjs.com/forum/showthread.php?t=5717&highlight=vtype
+    /**
+     * A validation type name as defined in {@link VType} (defaults to null)
+     *
+     * @param vtype the validation type
+     */
     public void setVtype(VType vtype) {
         JavaScriptObjectHelper.setAttribute(jsObj, "vtype", vtype.getVType());
     }
 
+    /**
+     * The validation type text if the validation specified by {@link #setVtype(VType)} fails.
+     *
+     * @param vtypeText
+     */
     public void setVtypeText(String vtypeText) {
         JavaScriptObjectHelper.setAttribute(jsObj, "vtypeText", vtypeText);
     }

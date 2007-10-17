f04b258d0f51bf0448147809fc8e147f68ab2242
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-17 04:33:14 +0000

    added javadocs

--- main/src/com/gwtext/client/widgets/grid/GridEditor.java
+++ main/src/com/gwtext/client/widgets/grid/GridEditor.java
@@ -24,8 +24,19 @@ import com.google.gwt.core.client.JavaScriptObject;
 import com.gwtext.client.core.JsObject;
 import com.gwtext.client.widgets.form.Field;
 
+/**
+ * Field Editor for a Grid column.
+ *
+ * @see com.gwtext.client.widgets.grid.ColumnConfig#setEditor(GridEditor)
+ * @see com.gwtext.client.widgets.grid.ColumnModel#setEditor(String, GridEditor)
+ */
 public class GridEditor extends JsObject {
 
+    /**
+     * Creates a new Grid Editor field.
+     *
+     * @param field the field
+     */
     public GridEditor(Field field) {
         jsObj = create(field.getJsObj());
     }

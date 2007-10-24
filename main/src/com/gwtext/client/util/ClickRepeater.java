04c590003a52517c998b433b98edee8e68ce79f5
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-24 02:09:13 +0000

    clickrepeater class

--- main/src/com/gwtext/client/util/ClickRepeater.java
+++ main/src/com/gwtext/client/util/ClickRepeater.java
@@ -20,5 +20,55 @@
 
 package com.gwtext.client.util;
 
-public class ClickRepeater {
+import com.google.gwt.core.client.JavaScriptObject;
+import com.gwtext.client.core.JsObject;
+import com.gwtext.client.util.event.ClickRepeaterListener;
+
+/**
+ * A wrapper class which can be applied to any element. Fires a "click" event while the mouse is pressed.
+ * The interval between firings may be specified in the config but defaults to 10 milliseconds. Optionally,
+ * a CSS class may be applied to the element during the time it is pressed.
+ */
+public class ClickRepeater extends JsObject {
+
+    /**
+     * Create a new ClickRepeater.
+     *
+     * @param config ClickRepeater configuration
+     */
+    public ClickRepeater(ClickRepeaterConfig config) {
+        jsObj = create(config.getJsObj());
+    }
+
+    private native JavaScriptObject create(JavaScriptObject config) /*-{
+        return new $wnd.Ext.util.ClickRepeater(config);
+    }-*/;
+
+    /**
+     * Add a ClickRepeater listener.
+     *
+     * @param listener the listener
+     */
+    public native void addListener(ClickRepeaterListener listener)/*-{
+        var cr = this.@com.gwtext.client.core.JsObject::jsObj;
+        var crJ = this;
+
+        cr.addListener('click',
+                function(source) {
+                    listener.@com.gwtext.client.util.event.ClickRepeaterListener::onClick(Lcom/gwtext/client/util/ClickRepeater;)(crJ);
+                }
+        );
+
+        cr.addListener('mousedown',
+                function(source) {
+                    listener.@com.gwtext.client.util.event.ClickRepeaterListener::onMouseDown(Lcom/gwtext/client/util/ClickRepeater;)(crJ);
+                }
+        );
+
+        cr.addListener('mouseup',
+                function(source) {
+                    listener.@com.gwtext.client.util.event.ClickRepeaterListener::onMouseUp(Lcom/gwtext/client/util/ClickRepeater;)(crJ);
+                }
+        );
+    }-*/;
 }

d2dc2db3ed81bf2351d1765db8a7bbabb4b330fe
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-24 02:22:04 +0000

    added javadocs

--- main/src/com/gwtext/client/util/DelayedTask.java
+++ main/src/com/gwtext/client/util/DelayedTask.java
@@ -23,8 +23,17 @@ import com.google.gwt.core.client.JavaScriptObject;
 import com.gwtext.client.core.Function;
 import com.gwtext.client.core.JsObject;
 
+/**
+ * Provides a convenient method of performing setTimeout where a new timeout cancels the old timeout.
+ * An example would be performing validation on a keypress. You can use this class to buffer the keypress events for a
+ * certain number of milliseconds, and perform only if they stop for that amount of time.
+ *
+ */
 public class DelayedTask extends JsObject {
 
+    /**
+     * Creates a new DelayedTask.
+     */
     public DelayedTask() {
         jsObj = create();
     }
@@ -33,11 +42,20 @@ public class DelayedTask extends JsObject {
         return new $wnd.Ext.util.DelayedTask();
     }-*/;
 
+    /**
+     * Cancel the last queued timeout.
+     */
     public native void cancel() /*-{
         var dtask = this.@com.gwtext.client.core.JsObject::jsObj;
         dtask.cancel();
     }-*/;
 
+    /**
+     * Cancels any pending timeout and queues a new one.
+     * 
+     * @param delay the milliseconds to delay
+     * @param task the task
+     */
     public native void delay(int delay, Function task) /*-{
         var dtask = this.@com.gwtext.client.core.JsObject::jsObj;
         dtask.delay(delay, function() {

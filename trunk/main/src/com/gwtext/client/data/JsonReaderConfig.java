7d52a1bfb1fedb9f634dcd706c61de98f4b96dd0
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-31 23:16:16 +0000

    updated javadocs

--- trunk/main/src/com/gwtext/client/data/JsonReaderConfig.java
+++ trunk/main/src/com/gwtext/client/data/JsonReaderConfig.java
@@ -23,20 +23,46 @@ package com.gwtext.client.data;
 import com.gwtext.client.core.BaseConfig;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 
+/**
+ * Configuration class for {@link com.gwtext.client.data.JsonReader}.
+ *
+ * @see com.gwtext.client.data.JsonReader
+ */
 public class JsonReaderConfig extends BaseConfig {
 
+    /**
+     * Name of the property within a row object that contains a record identifier value.
+     *
+     * @param id the id property
+     */
     public void setId(String id) {
         JavaScriptObjectHelper.setAttribute(jsObj, "id", id);
     }
 
+    /**
+     * Name of the property which contains the Array of row objects.
+     * 
+     * @param root the root property
+     */
     public void setRoot(String root) {
         JavaScriptObjectHelper.setAttribute(jsObj, "root", root);
     }
 
+    /**
+     * Name of the property from which to retrieve the success attribute used by forms.
+     *
+     * @param successProperty the success property
+     */
     public void setSuccessProperty(String successProperty) {
         JavaScriptObjectHelper.setAttribute(jsObj, "successProperty", successProperty);
     }
 
+    /**
+     * Name of the property from which to retrieve the total number of records in the dataset.
+     * This is only needed if the whole dataset is not passed in one go, but is being paged from the remote server.
+     *
+     * @param totalProperty proeprty for total number of records
+     */
     public void setTotalProperty(String totalProperty) {
         JavaScriptObjectHelper.setAttribute(jsObj, "totalProperty", totalProperty);
     }

80aa9d2d73a1595331792bd211ced341c4b6665e
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-31 02:46:42 +0000

    polishing / javadocs

--- trunk/main/src/com/gwtext/client/core/EventCallback.java
+++ trunk/main/src/com/gwtext/client/core/EventCallback.java
@@ -19,7 +19,17 @@
  */
 package com.gwtext.client.core;
 
+/**
+ * Event callback interface. The {@link EventObject} as a result of the event is passed in the callback.
+ *
+ * @see com.gwtext.client.core.BaseElement#addListener(String, EventCallback)
+ */
 public interface EventCallback {
 
+    /**
+     * Callback method.
+     *
+     * @param e the event object
+     */
     void execute(EventObject e);
 }

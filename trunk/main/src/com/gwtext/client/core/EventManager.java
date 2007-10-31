2e22d765bed6bfe6b59046f41e17fe121f6015e2
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-31 02:53:37 +0000

    polishing / javadocs

--- trunk/main/src/com/gwtext/client/core/EventManager.java
+++ trunk/main/src/com/gwtext/client/core/EventManager.java
@@ -22,8 +22,20 @@ package com.gwtext.client.core;
 import com.google.gwt.user.client.Element;
 import com.gwtext.client.core.event.TextResizeListener;
 
+/**
+ * Registers event handlers that want to receive a normalized EventObject instead of the standard browser event and
+ * provides several useful events directly.
+ */
 public class EventManager {
 
+    /**
+     * Appends an event handler to an element.
+     *
+     * @param element the element
+     * @param eventName the event name
+     * @param cb the callback to execute on event
+     * @param config the listener config 
+     */
     public static native void addListener(Element element, String eventName, EventCallback cb, ListenerConfig config) /*-{        
         $wnd.Ext.EventManager.addListener(element, eventName, function(event) {
                 var e = (event === undefined || event == null) ? null : @com.gwtext.client.core.EventObject::instance(Lcom/google/gwt/core/client/JavaScriptObject;)(event);
@@ -34,8 +46,17 @@ public class EventManager {
         );
     }-*/;
 
-    //excluded onDocumentReady
-    //handled by GWT and module entry point machanism
+    /**
+     * Fires when the document is ready (before onload and before images are loaded).
+     * Can alternatively use the GWT entry point mechanism.
+     * 
+     * @param cb callback to execute
+     */
+    public static native void onDocumentReady(Function cb) /*-{
+        $wnd.Ext.EventManager.onDocumentReady(function() {
+                cb.@com.gwtext.client.core.Function::execute()();
+            });
+    }-*/;
 
     // excluded onWindowResize
     // Use com.google.gwt.user.client.WindowResizeListener instead

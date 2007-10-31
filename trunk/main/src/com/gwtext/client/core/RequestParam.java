cd7c3fff27f9b457b3e00ed2de908d07bdcf1bc0
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-31 03:34:24 +0000

    updated javadocs

--- trunk/main/src/com/gwtext/client/core/RequestParam.java
+++ trunk/main/src/com/gwtext/client/core/RequestParam.java
@@ -23,33 +23,68 @@ package com.gwtext.client.core;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 
+/**
+ * This class represents a HTTP Request.
+ *
+ * @see com.gwtext.client.core.Connection#request(RequestParam)
+ */
 public class RequestParam extends JsObject {
 
     public RequestParam() {
         jsObj = JavaScriptObjectHelper.createObject();
     }
 
+    /**
+     * The URL of the request.
+     *
+     * @param url the rul
+     */
     public void setUrl(boolean url) {
         JavaScriptObjectHelper.setAttribute(jsObj, "url", url);
     }
 
+    /**
+     * The request paameters.
+     *
+     * @param params the request parameters
+     */
     public void setParams(UrlParam[] params) {
         JavaScriptObject paramObj = UrlParam.getJsObj(params);
         JavaScriptObjectHelper.setAttribute(jsObj, "params", paramObj);
     }
 
-    public void setMethod(String method) {
-        JavaScriptObjectHelper.setAttribute(jsObj, "method", method);
+    /**
+     * The request method (GET or POST).
+     *
+     * @param method the request method
+     */
+    public void setMethod(Connection.Method method) {
+        JavaScriptObjectHelper.setAttribute(jsObj, "method", method.getMethod());
     }
 
+    /**
+     * The ID of the form that you want to use. The request parameters will include the Form's field names and values.
+     *
+     * @param formId the form ID
+     */
     public void setForm(String formId) {
         JavaScriptObjectHelper.setAttribute(jsObj, "form", formId);
     }
 
+    /**
+     * Does the form do a file upload.
+     *
+     * @param isUpload true if file upload
+     */
     public void setIsUpload(boolean isUpload) {
         JavaScriptObjectHelper.setAttribute(jsObj, "isUpload", isUpload);
     }
 
+    /**
+     * The headers to pass with the request.
+     *
+     * @param defaultHeaders the headers
+     */
     public void setHeaders(NameValuePair[] defaultHeaders) {
         JavaScriptObject paramObj = NameValuePair.getJsObj(defaultHeaders);
         JavaScriptObjectHelper.setAttribute(jsObj, "headers", paramObj);

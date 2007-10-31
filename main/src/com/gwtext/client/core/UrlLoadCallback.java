69345c7067463f7423a6eade9c810e3fb2ba0e88
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-31 04:36:35 +0000

    updated javadocs

--- main/src/com/gwtext/client/core/UrlLoadCallback.java
+++ main/src/com/gwtext/client/core/UrlLoadCallback.java
@@ -20,7 +20,20 @@
 
 package com.gwtext.client.core;
 
+/**
+ * Callback interface used in various methods of {@link com.gwtext.client.core.UpdateManager}.
+ *
+ * @see com.gwtext.client.core.UpdateManager#formUpdate(String, String, boolean, UrlLoadCallback)
+ * @see com.gwtext.client.core.UpdateManager#update(String, UpdateManagerConfig, UrlLoadCallback, boolean)
+ */
 public interface UrlLoadCallback {
 
+    /**
+     * The callback method.
+     *
+     * @param success true if request is successful
+     * @param httpStatus the http status code
+     * @param responseText thre response text
+     */
     void execute(boolean success, int httpStatus, String responseText);
 }

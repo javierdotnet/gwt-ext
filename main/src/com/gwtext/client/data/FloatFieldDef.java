5b36cfad595fe4798232e88138dfec71adfcf565
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-31 22:32:42 +0000

    updated javadocs

--- main/src/com/gwtext/client/data/FloatFieldDef.java
+++ main/src/com/gwtext/client/data/FloatFieldDef.java
@@ -23,24 +23,59 @@ package com.gwtext.client.data;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 
+/**
+ * Field that represents float data.
+ *
+ */
 public class FloatFieldDef extends FieldDef {
 
+    /**
+     * Construct a new FloatFieldDef.
+     *
+     * @param name the field name
+     */
     public FloatFieldDef(String name) {
         this(name, null, null);
     }
 
+    /**
+     * Construct a new FloatFieldDef.
+     *
+     * @param name the field name
+     * @param mapping the field mapping. Depending on the Reader used, mapping could be the array index position or an XPath expression when reading from XML
+     */
     public FloatFieldDef(String name, String mapping) {
         this(name, mapping, null);
     }
 
+    /**
+     * Construct a new FloatFieldDef.
+     *
+     * @param name the field name
+     * @param mapping the field mapping. Depending on the Reader used, mapping could be the array index position or an XPath expression when reading from XML
+     */
     public FloatFieldDef(String name, int mapping) {
         this(name, mapping, null);
     }
 
+    /**
+     * Construct a new FloatFieldDef.
+     *
+     * @param name the field name
+     * @param mapping the field mapping. Depending on the Reader used, mapping could be the array index position or an XPath expression when reading from XML
+     * @param converter the field converter
+     */
     public FloatFieldDef(String name, String mapping, Converter converter) {
         jsObj = create(name, mapping, converter);
     }
 
+    /**
+     * Construct a new FloatFieldDef.
+     *
+     * @param name the field name
+     * @param mapping the field mapping. Depending on the Reader used, mapping could be the array index position or an XPath expression when reading from XML
+     * @param converter the field converter
+     */
     public FloatFieldDef(String name, int mapping, Converter converter) {
         this(name, String.valueOf(mapping), converter);
     }

8c4be55e260ecc744f8657d257c11d05e11b0dc8
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-31 23:14:13 +0000

    updated javadocs

--- main/src/com/gwtext/client/data/JsonReader.java
+++ main/src/com/gwtext/client/data/JsonReader.java
@@ -22,6 +22,32 @@ package com.gwtext.client.data;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
+/**
+ * Data reader class to create an Array of Ext.data.Record objects from a JSON response
+ * based on mappings in a provided Ext.data.Record constructor.
+ * <p>
+ * Example code:
+ * </p><pre><code>
+ *  RecordDef recordDef = new RecordDef(new FieldDef[]{
+ *             new StringFieldDef("name", "name"), // "mapping" property not needed if it's the same as "name"
+ *             new StringFieldDef("occupation")    // this field will use "occupation" as the mapping.
+ *     });
+ *
+ *  JsonReader reader = new JsonReader(new JsonReaderConfig() {
+ *      {
+ *          setTotalProperty("results"); // The property which contains the total dataset size (optional)
+ *          setRoot("rows");             // The property which contains an Array of row objects
+ *          setId("id");                 // The property within each row object that provides an ID for the record (optional)
+ *      }}, recordDef);
+ * </code>
+ * </pre>
+ * <p>
+ * This would consume a JSON file like this:
+ * </p><pre><code>{ <em>'results'</em>: 2, <em>'rows'</em>: [
+ *     { <em>'id'</em>: 1, <em>'name'</em>: <em>'Bill'</em>, occupation: <em>'Gardener'</em> },
+ *     { <em>'id'</em>: 2, <em>'name'</em>: <em>'Ben'</em>, occupation: <em>'Horticulturalist'</em> } ]
+ * }</code></pre>        
+ */
 public class JsonReader extends Reader {
 
     public JsonReader(JsonReaderConfig config, RecordDef recordDef) {

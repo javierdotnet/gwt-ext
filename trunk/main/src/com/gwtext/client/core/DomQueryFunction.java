588956c6cf4e862992ed8bcb1ac3aff80ab7e3d6
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-31 02:44:10 +0000

    polishing / javadocs

--- trunk/main/src/com/gwtext/client/core/DomQueryFunction.java
+++ trunk/main/src/com/gwtext/client/core/DomQueryFunction.java
@@ -22,9 +22,26 @@ package com.gwtext.client.core;
 
 import com.google.gwt.user.client.Element;
 
+/**
+ * Intefrace used / returned by certain {@link com.gwtext.client.core.DomQuery} methods.
+ *
+ * @see com.gwtext.client.core.DomQuery#compile(String)
+ * @see com.gwtext.client.core.DomQuery#compile(String, com.gwtext.client.core.DomQuery.SelectorType)
+ */
 public interface DomQueryFunction {
 
+    /**
+     * Execute the query represented by this function.
+     *
+     * @return result of query
+     */
     Element[] query();
 
+    /**
+     * Execute the query represented by this function starting with the specified root element.
+     *
+     * @param root the root element
+     * @return result of query
+     */
     Element[] query(Element root);
 }

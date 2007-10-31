4b4b867d6b0a60740f56d3b0b3b684faab5a71c7
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-10-31 03:36:56 +0000

    updated javadocs

--- trunk/main/src/com/gwtext/client/core/Size.java
+++ trunk/main/src/com/gwtext/client/core/Size.java
@@ -23,6 +23,11 @@ package com.gwtext.client.core;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 
+/**
+ * This class represents the size of an element.
+ *
+ * @see ExtElement#getSize()
+ */
 public class Size {
 
     private float height;
@@ -31,30 +36,48 @@ public class Size {
     public Size() {
     }
 
+    /**
+     * Create a new instance.
+     *
+     * @param width element width
+     * @param height element height
+     */
     public Size(int width, int height) {
         this.width = width;
         this.height = height;
     }
 
-    public static Size instance(JavaScriptObject jsObj) {
+    private static Size instance(JavaScriptObject jsObj) {
         return new Size(
                 JavaScriptObjectHelper.getAttributeAsInt(jsObj, "width"),
                 JavaScriptObjectHelper.getAttributeAsInt(jsObj, "height")
         );
     }
 
+    /**
+     * @return the height
+     */
     public float getHeight() {
         return height;
     }
 
+    /**
+     * @param height the height
+     */
     public void setHeight(float height) {
         this.height = height;
     }
 
+    /**
+     * @return the width
+     */
     public float getWidth() {
         return width;
     }
 
+    /**
+     * @param width the width
+     */
     public void setWidth(float width) {
         this.width = width;
     }

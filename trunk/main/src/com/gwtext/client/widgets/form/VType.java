9083905263438c2ad8fb6567977f9b2dcf920e41
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-02 02:59:14 +0000

    updates, issue 143

--- trunk/main/src/com/gwtext/client/widgets/form/VType.java
+++ trunk/main/src/com/gwtext/client/widgets/form/VType.java
@@ -20,10 +20,13 @@
 
 package com.gwtext.client.widgets.form;
 
+import com.gwtext.client.core.RegExp;
+
 
 //http://extjs.com/forum/showthread.php?t=4271&highlight=vtype
 /**
- * Basic pre-created validation types.
+ * Basic pre-created validation types. Overridable validation definitions. The validations provided are basic and
+ * intended to be easily customizable and extended.
  *
  * @see com.gwtext.client.widgets.form.TextFieldConfig#setVtype(VType) 
  */
@@ -31,7 +34,7 @@ public class VType {
 
     private String vType;
 
-    public VType(String vType) {
+    private VType(String vType) {
         this.vType = vType;
     }
 
@@ -39,15 +42,90 @@ public class VType {
         return vType;
     }
 
+    /**
+     * The function used to validate alpha values
+     */
     public static VType ALPHA = new VType("alpha");
-    public static VType ALPHA_MASK = new VType("alphaMask");
-    public static VType ALPHA_TEXT = new VType("alphaText");
-    public static VType ALPHANUM_MASK = new VType("alphanumMask");
+
+    /**
+     * The function used to validate alphanumeric values.
+     */
     public static VType ALPHANUM = new VType("alphanum");
-    public static VType ALPHANUM_TEXT = new VType("alphanumText");
+
+    /**
+     * The function used to validate email addresses.
+     */
     public static VType EMAIL = new VType("email");
-    public static VType EMAIL_MASK = new VType("emailMask");
-    public static VType EMAIL_TEXT = new VType("emailText");
+
+    /**
+     * The function used to validate URLs.
+     */
     public static VType URL = new VType("url");
-    public static VType URL_TEXT = new VType("urlText");
+
+    /**
+     * The error text to display when the alpha validation function returns false.
+     *
+     * @param alphaText error message
+     */
+    public static native void setAlphaText(String alphaText)/*-{
+         $wnd.Ext.form.VTypes.alphaText = alphaText;
+    }-*/;
+
+    /**
+     * The keystroke filter mask to be applied on alpha input.
+     *
+     * @param alphaMask the alpha mask
+     */
+    public static native void setAlphaMask(RegExp alphaMask)/*-{
+        var re = alphaMask.@com.gwtext.client.core.JsObject::jsObj;
+        $wnd.Ext.form.VTypes.alphaMask = re;
+    }-*/;
+
+    /**
+     * The error text to display when the alphanumeric validation function returns false.
+     * 
+     * @param alphanumText the error message
+     */
+    public static native void setAlphanumText(String alphanumText)/*-{
+         $wnd.Ext.form.VTypes.alphanumText = alphanumText;
+    }-*/;
+
+    /**
+     * The keystroke filter mask to be applied on alphanumeric input.
+     *
+     * @param alphanumMask the alpha num mask
+     */
+    public static native void setAlphanumMask(RegExp alphanumMask)/*-{
+        var re = alphanumMask.@com.gwtext.client.core.JsObject::jsObj;
+        $wnd.Ext.form.VTypes.alphanumMask = re;
+    }-*/;
+
+    /**
+     * The error text to display when the email validation function returns false.
+     * 
+     * @param emailText the error message
+     */
+    public static native void setEmailText(String emailText)/*-{
+         $wnd.Ext.form.VTypes.emailText = emailText;
+    }-*/;
+
+    /**
+     * The keystroke filter mask to be applied on email input.
+     *
+     * @param emailMask the email mask
+     */
+    public static native void setEmailMask(RegExp emailMask)/*-{
+        var re = emailMask.@com.gwtext.client.core.JsObject::jsObj;
+        $wnd.Ext.form.VTypes.emailMask = re;
+    }-*/;
+
+    /**
+     * The error text to display when the url validation function returns false.
+     * 
+     * @param urlText the error message
+     */
+    public static native void setUrlText(String urlText)/*-{
+         $wnd.Ext.form.VTypes.urlText = urlText;
+    }-*/;
+
 }

582cf755f0f76a3d701824949d02f4366fa7d825
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-03 03:30:56 +0000

    updated javadocs

--- trunk/main/src/com/gwtext/client/util/KeyNav.java
+++ trunk/main/src/com/gwtext/client/util/KeyNav.java
@@ -23,16 +23,37 @@ import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.user.client.Element;
 import com.gwtext.client.core.JsObject;
 
+/**
+ *
+ * Provides a convenient wrapper for normalized keyboard navigation. KeyNav allows you to bind navigation keys to function
+ * calls that will get called when the keys are pressed, providing an easy way to implement custom navigation schemes for any UI component.
+ * <p>
+ * The following are all of the possible keys that can be implemented: enter, left, right, up, down, tab, esc, pageUp, pageDown, del, home, end.
+ *
+ * @see com.gwtext.client.util.KeyNavConfig
+ */
 public class KeyNav extends JsObject {
 
     public KeyNav(JavaScriptObject jsObj) {
         super(jsObj);
     }
 
+    /**
+     * Creates a new KeyNav isntance.
+     *
+     * @param id the element ID
+     * @param config the keynav configuration
+     */
     public KeyNav(String id, KeyNavConfig config) {
         jsObj = create(id, config.getJsObj());
     }
 
+    /**
+     * Creates a new KeyNav isntance.
+     *
+     * @param el the element
+     * @param config the keynav configuration
+     */
     public KeyNav(Element el, KeyNavConfig config) {
         jsObj = create(el, config.getJsObj());
     }
@@ -45,11 +66,17 @@ public class KeyNav extends JsObject {
         return new  $wnd.Ext.KeyNav(id, config);
     }-*/;
 
+    /**
+     * Disable the KeyNav.
+     */
     public native void disable() /*-{
         var kn = this.@com.gwtext.client.core.JsObject::jsObj;
         kn.disable();
     }-*/;
 
+    /**
+     * Enable the KeyNav.
+     */
     public native void enable() /*-{
         var kn = this.@com.gwtext.client.core.JsObject::jsObj;
         kn.enable();

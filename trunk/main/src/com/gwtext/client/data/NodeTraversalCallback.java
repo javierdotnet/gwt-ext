6bcdc45402fca11e311f5cd6b553df362d1bf847
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-03 00:00:00 +0000

    updated javadocs

--- trunk/main/src/com/gwtext/client/data/NodeTraversalCallback.java
+++ trunk/main/src/com/gwtext/client/data/NodeTraversalCallback.java
@@ -20,7 +20,22 @@
 
 package com.gwtext.client.data;
 
+/**
+ * The node traversal callback itnerface used by several Node methods.
+ *
+ * @see Node#cascade(NodeTraversalCallback)
+ * @see Node#bubble(NodeTraversalCallback)
+ * @see Node#findChildBy(NodeTraversalCallback) 
+ *
+ */
 public interface NodeTraversalCallback {
 
+    /**
+     * The callback method.
+     *
+     * @param node the node beign traversed upon.
+     *
+     * @return boolean value
+     */
     boolean execute(Node node);
 }

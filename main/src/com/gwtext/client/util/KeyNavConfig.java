3e5f0bb10517293037007a603600a0f5e8381ab3
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-03 03:29:07 +0000

    updated javadocs

--- main/src/com/gwtext/client/util/KeyNavConfig.java
+++ main/src/com/gwtext/client/util/KeyNavConfig.java
@@ -22,8 +22,16 @@ package com.gwtext.client.util;
 import com.gwtext.client.core.BaseConfig;
 import com.gwtext.client.core.EventCallback;
 
+/**
+ * KeyNav configuration class.
+ */
 public class KeyNavConfig extends BaseConfig {
 
+    /**
+     * Register handler for the Enter key.
+     *
+     * @param cb the key handler
+     */
     public native void onEnter(EventCallback cb) /*-{
         var config = this.@com.gwtext.client.core.JsObject::jsObj;
         config['enter'] = function(e) {
@@ -32,6 +40,11 @@ public class KeyNavConfig extends BaseConfig {
         };
     }-*/;
 
+    /**
+     * Register handler for the Left arrow key.
+     *
+     * @param cb the key handler
+     */
     public native void onLeft(EventCallback cb) /*-{
         var config = this.@com.gwtext.client.core.JsObject::jsObj;
         config['left'] = function(e) {
@@ -40,6 +53,11 @@ public class KeyNavConfig extends BaseConfig {
         };
     }-*/;
 
+    /**
+     * Register handler for the Right arrow key.
+     *
+     * @param cb the key handler
+     */
     public native void onRight(EventCallback cb) /*-{
         var config = this.@com.gwtext.client.core.JsObject::jsObj;
         config['right'] = function(e) {
@@ -48,6 +66,11 @@ public class KeyNavConfig extends BaseConfig {
         };
     }-*/;
 
+    /**
+     * Register handler for the Up arrow key.
+     *
+     * @param cb the key handler
+     */
     public native void onUp(EventCallback cb) /*-{
         var config = this.@com.gwtext.client.core.JsObject::jsObj;
         config['up'] = function(e) {
@@ -56,6 +79,11 @@ public class KeyNavConfig extends BaseConfig {
         };
     }-*/;
 
+    /**
+     * Register handler for the Down arrow key.
+     *
+     * @param cb the key handler
+     */
     public native void onDown(EventCallback cb) /*-{
         var config = this.@com.gwtext.client.core.JsObject::jsObj;
         config['down'] = function(e) {
@@ -64,6 +92,11 @@ public class KeyNavConfig extends BaseConfig {
         };
     }-*/;
 
+    /**
+     * Register handler for the Tab key.
+     *
+     * @param cb the key handler
+     */
     public native void onTab(EventCallback cb) /*-{
         var config = this.@com.gwtext.client.core.JsObject::jsObj;
         config['tab'] = function(e) {
@@ -72,6 +105,11 @@ public class KeyNavConfig extends BaseConfig {
         };
     }-*/;
 
+    /**
+     * Register handler for the Esc key.
+     *
+     * @param cb the key handler
+     */
     public native void onEsc(EventCallback cb) /*-{
         var config = this.@com.gwtext.client.core.JsObject::jsObj;
         config['esc'] = function(e) {
@@ -80,6 +118,11 @@ public class KeyNavConfig extends BaseConfig {
         };
     }-*/;
 
+    /**
+     * Register handler for the PageUp key.
+     *
+     * @param cb the key handler
+     */
     public native void onPageUp(EventCallback cb) /*-{
         var config = this.@com.gwtext.client.core.JsObject::jsObj;
         config['pageUp'] = function(e) {
@@ -88,6 +131,11 @@ public class KeyNavConfig extends BaseConfig {
         };
     }-*/;
 
+    /**
+     * Register handler for the PageDown key.
+     *
+     * @param cb the key handler
+     */
     public native void onPageDown(EventCallback cb) /*-{
         var config = this.@com.gwtext.client.core.JsObject::jsObj;
         config['pageDown'] = function(e) {
@@ -96,6 +144,11 @@ public class KeyNavConfig extends BaseConfig {
         };
     }-*/;
 
+    /**
+     * Register handler for the Del key.
+     *
+     * @param cb the key handler
+     */
     public native void onDel(EventCallback cb) /*-{
         var config = this.@com.gwtext.client.core.JsObject::jsObj;
         config['del'] = function(e) {
@@ -104,6 +157,11 @@ public class KeyNavConfig extends BaseConfig {
         };
     }-*/;
 
+    /**
+     * Register handler for the Home key.
+     *
+     * @param cb the key handler
+     */
     public native void onHome(EventCallback cb) /*-{
         var config = this.@com.gwtext.client.core.JsObject::jsObj;
         config['home'] = function(e) {
@@ -112,6 +170,11 @@ public class KeyNavConfig extends BaseConfig {
         };
     }-*/;
 
+    /**
+     * Register handler for the End key.
+     *
+     * @param cb the key handler
+     */
     public native void onEnd(EventCallback cb) /*-{
         var config = this.@com.gwtext.client.core.JsObject::jsObj;
         config['end'] = function(e) {

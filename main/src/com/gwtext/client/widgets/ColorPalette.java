ef3f66c27f1f1ab01dd7ec5b120428fe29be5923
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-03 04:18:00 +0000

    updated javadocs

--- main/src/com/gwtext/client/widgets/ColorPalette.java
+++ main/src/com/gwtext/client/widgets/ColorPalette.java
@@ -24,12 +24,20 @@ import com.google.gwt.core.client.JavaScriptObject;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 import com.gwtext.client.widgets.event.ColorPaletteListener;
 
+/**
+ * Simple color palette class for choosing colors. 
+ */
 public class ColorPalette extends Component {
 
     public ColorPalette(JavaScriptObject jsObj) {
         super(jsObj);
     }
 
+    /**
+     * Create a new ColorPalette.
+     *
+     * @param config the ColorPalette config
+     */
     public ColorPalette(ColorPaletteConfig config) {
         jsObj = create(config.getJsObj());
     }
@@ -39,6 +47,11 @@ public class ColorPalette extends Component {
         return cp;
     }-*/;
 
+    /**
+     * Selects the specified color in the palette (fires the select event).
+     *
+     * @param color A valid 6-digit color hex code (# will be stripped if included)
+     */
     public native void select(String color) /*-{
         var cp = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         cp.select(color);
@@ -48,14 +61,32 @@ public class ColorPalette extends Component {
         return new ColorPalette(jsObj);
     }
 
+    /**
+     * An array of 6-digit color hex code strings (without the # symbol). This array can contain any number of colors,
+     * and each hex code should be unique. The width of the palette is controlled via CSS by adjusting the width property
+     * of the 'x-color-palette' class (or assigning a custom class), so you can balance the number of colors with the width
+     * setting until the box is symmetrical.
+     * 
+     * @return the colors array
+     */
     public String[] getColors() {
         return JavaScriptObjectHelper.getAttributeAsStringArray(jsObj, "colors");
     }
 
+    /**
+     * Set the colors array.
+     *
+     * @param colors the colors array
+     */
     public void setColors(String[] colors) {
         JavaScriptObjectHelper.setAttribute(jsObj, "colors", JavaScriptObjectHelper.convertToJavaScriptArray(colors));
     }
 
+    /**
+     * Add a ColorPalette listener.
+     *
+     * @param listener the listener
+     */
     public native void addColorPaletteListener(ColorPaletteListener listener) /*-{
         var componentJ = this;
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;

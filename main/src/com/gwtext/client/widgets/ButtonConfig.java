9f2919ef3c841bf15bda086606e61465d7a09bf2
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-03 04:09:33 +0000

    updated javadocs

--- main/src/com/gwtext/client/widgets/ButtonConfig.java
+++ main/src/com/gwtext/client/widgets/ButtonConfig.java
@@ -20,12 +20,17 @@
 
 package com.gwtext.client.widgets;
 
+import com.gwtext.client.core.AnchorPosition;
 import com.gwtext.client.core.BaseConfig;
 import com.gwtext.client.core.Template;
+import com.gwtext.client.util.ClickRepeaterConfig;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 import com.gwtext.client.widgets.event.ButtonListener;
 import com.gwtext.client.widgets.menu.Menu;
 
+/**
+ * Configuration class for a {@link Button}.
+ */
 public class ButtonConfig extends BaseConfig {
 
     private ButtonListener buttonListener;
@@ -35,14 +40,29 @@ public class ButtonConfig extends BaseConfig {
         JavaScriptObjectHelper.setAttribute(jsObj, "autoCreate", autoCreate);
     }
 
+    /**
+     * A CSS class to apply to the button's main element.
+     *
+     * @param cls a CSS class
+     */
     public void setCls(String cls) {
         JavaScriptObjectHelper.setAttribute(jsObj, "cls", cls);
     }
 
+    /**
+     * True to start disabled (defaults to false).
+     *
+     * @param disabled
+     */
     public void setDisabled(boolean disabled) {
         JavaScriptObjectHelper.setAttribute(jsObj, "disabled", disabled);
     }
 
+    /**
+     * True to enable pressed/not pressed toggling (defaults to false).
+     * 
+     * @param enableToggle true to enable toggle
+     */
     public void setEnableToggle(boolean enableToggle) {
         JavaScriptObjectHelper.setAttribute(jsObj, "enableToggle", enableToggle);
     }
@@ -57,49 +77,100 @@ public class ButtonConfig extends BaseConfig {
        JavaScriptObjectHelper.setAttribute(jsObj, "formBind", bindToForm);
     }
 
+    /**
+     * False to disable visual cues on mouseover, mouseout and mousedown (defaults to true).
+     *
+     * @param handleMouseEvents false to diable visual cues
+     */
     public void setHandleMouseEvents(boolean handleMouseEvents) {
         JavaScriptObjectHelper.setAttribute(jsObj, "handleMouseEvents", handleMouseEvents);
     }
 
+    /**
+     * True to start hidden (defaults to false).
+     *
+     * @param hidden true for hidden
+     */
     public void setHidden(boolean hidden) {
         JavaScriptObjectHelper.setAttribute(jsObj, "hidden", hidden);
     }
 
     //TODO doesnt seem to be working for toolbar menu button
+    /**
+     * The path to an image to display in the button (the image will be set as the background-image CSS property of the
+     * button by default, so if you want a mixed icon/text button, set cls:"x-btn-text-icon")
+     *
+     * @param icon the icon
+     */
     public void setIcon(String icon) {
         JavaScriptObjectHelper.setAttribute(jsObj, "icon", icon);
         //setCls("x-btn-icon");
     }
 
+    /**
+     * A css class which sets a background image to be used as the icon for this button (defaults to undefined).
+     *
+     * @param iconCls the icon CSS class
+     */
     public void setIconCls(String iconCls) {
         JavaScriptObjectHelper.setAttribute(jsObj, "iconCls", iconCls);
     }
 
+    /**
+     * Assign a button menu.
+     * 
+     * @param menu the button menu
+     */
     public void setMenu(Menu menu) {
         JavaScriptObjectHelper.setAttribute(jsObj, "menu", menu.getJsObj());
     }
 
-    //todo make typesafe with constants
-    public void setMenuAlign(String menuAlign) {
-        JavaScriptObjectHelper.setAttribute(jsObj, "menuAlign", menuAlign);
+
+    /**
+     * The position to align the menu to.
+     *
+     * @param elementAnchor the element's anchor point
+     * @param targetAnchor the target's anchor point
+     */
+    public void setMenuAlign(AnchorPosition elementAnchor, AnchorPosition targetAnchor) {
+        JavaScriptObjectHelper.setAttribute(jsObj, "menuAlign", elementAnchor.getPosition() +"-" + targetAnchor.getPosition());
     }
 
+    /**
+     * The minimum width for this button.
+     *
+     * @param minWidth the min width
+     */
     public void setMinWidth(int minWidth) {
         JavaScriptObjectHelper.setAttribute(jsObj, "minWidth", minWidth);
     }
 
+    /**
+     * True to start pressed (only if enableToggle = true).
+     *
+     * @param pressed true for pressed
+     */
     public void setPressed(boolean pressed) {
         JavaScriptObjectHelper.setAttribute(jsObj, "pressed", pressed);
     }
 
+    /**
+     * True to repeat fire the click event while the mouse is down. (defaults to false).
+     *
+     * @param repeat
+     */
     public void setRepeat(boolean repeat) {
         JavaScriptObjectHelper.setAttribute(jsObj, "repeat", repeat);
     }
 
-    //TODO  setRepeat
-    /*public void setRepeat(ClickRepeater config) {
-        //JavaScriptObjectHelper.setAttribute(jsObj, "config", config);
-    }*/
+    /**
+     * Fire click event when mouse is down.
+     *
+     * @param config the click repeat configuration
+     */
+    public void setRepeat(ClickRepeaterConfig config) {
+        JavaScriptObjectHelper.setAttribute(jsObj, "config", config.getJsObj());
+    }
 
     public void setTabIndex(int tabIndex) {
         JavaScriptObjectHelper.setAttribute(jsObj, "tabIndex", tabIndex);
@@ -110,18 +181,39 @@ public class ButtonConfig extends BaseConfig {
         JavaScriptObjectHelper.setAttribute(jsObj, "template", template.getJsObj());
     }
 
+    /**
+     * The button text.
+     *
+     * @param text the buttons text
+     */
     public void setText(String text) {
         JavaScriptObjectHelper.setAttribute(jsObj, "text", text);
     }
 
+    /**
+     * The group this toggle button is a member of (only 1 per group can be pressed, only applies if enableToggle = true).
+     *
+     * @param toggleGroup the button's toggle group
+     */
     public void setToggleGroup(String toggleGroup) {
         JavaScriptObjectHelper.setAttribute(jsObj, "toggleGroup", toggleGroup);
     }
 
+    /**
+     *  The tooltip for the button.
+     *
+     * @param tooltip the tooltip
+     */
     public void setTooltip(String tooltip) {
         JavaScriptObjectHelper.setAttribute(jsObj, "tooltip", tooltip);
     }
 
+    /**
+     *  The tooltip for the button.
+     *
+     * @param title the tootlip title
+     * @param text the tootlip text
+     */
     public void setTooltip(final String title, final String text) {
         JavaScriptObjectHelper.setAttribute(jsObj, "tooltip", new QuickTipsConfig() {
             {
@@ -131,15 +223,33 @@ public class ButtonConfig extends BaseConfig {
         }.getJsObj());
     }
 
+    /**
+     * The tooltip for the button.
+     *
+     * @param config the tooltip config
+     */
     public void setTooltip(QuickTipsConfig config) {
         JavaScriptObjectHelper.setAttribute(jsObj, "tooltip", config.getJsObj());
     }
 
-    //TODO make typesafe  "qtip" or "title"
-    public void setTooltipType(String tooltipType) {
-        JavaScriptObjectHelper.setAttribute(jsObj, "tooltipType", tooltipType);
+    /**
+     * The type of tooltip to use.
+     *
+     * @param quickTip true for quicktips, false for the "title" attribute.
+     */
+    public void setTooltipType(boolean quickTip) {
+        if(quickTip){
+            JavaScriptObjectHelper.setAttribute(jsObj, "tooltipType", "qtip");
+        } else {
+            JavaScriptObjectHelper.setAttribute(jsObj, "tooltipType", "title");
+        }
     }
 
+    /**
+     * The button's type, corresponding to the DOM input element type attribute. Either "submit," "reset" or "button" (default).
+     * 
+     * @param type the button type
+     */
     public void setType(String type) {
         JavaScriptObjectHelper.setAttribute(jsObj, "type", type);
     }
@@ -148,6 +258,11 @@ public class ButtonConfig extends BaseConfig {
         return buttonListener;
     }
 
+    /**
+     * Add a Button Listener.
+     *
+     * @param buttonListener the listener
+     */
     public void setButtonListener(ButtonListener buttonListener) {
         this.buttonListener = buttonListener;
     }

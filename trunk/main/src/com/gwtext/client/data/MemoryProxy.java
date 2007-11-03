1a6d37a48d2c8010cc4893b728a98034d3d34e0f
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-03 00:06:37 +0000

    updated javadocs

--- trunk/main/src/com/gwtext/client/data/MemoryProxy.java
+++ trunk/main/src/com/gwtext/client/data/MemoryProxy.java
@@ -23,14 +23,43 @@ package com.gwtext.client.data;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 
-
+/**
+ * An implementation of DataProxy that simply passes the data specified in its constructor to the Reader when its load method is called.
+ *
+ * <pre>
+ * <code>
+ *
+ * Object[][] states = new Object[][]{
+ *               new Object[]{"AL", "Alabama"},
+ *               new Object[]{"AK", "Alaska"},
+ *               new Object[]{"AZ", "Arizona"},
+ *               new Object[]{"AR", "Arkansas"},
+ *               new Object[]{"CA", "California"}};
+ *
+ * Reader reader = new ArrayReader(new RecordDef(
+ *                new FieldDef[]{
+ *                       new StringFieldDef("abbr"),
+ *                       new StringFieldDef("state")
+ *               }));
+ *
+ * Store store = new Store(proxy, reader);
+ * </code>
+ * </pre>
+ * 
+ */
 public class MemoryProxy extends DataProxy {
 
+    /**
+     *
+     * Create s new memory proxy using the passed array data.
+     *
+     * @param data the array data
+     */
     public MemoryProxy(Object[][] data) {
         jsObj = create(JavaScriptObjectHelper.convertToJavaScriptArray(data));
     }
 
-    protected native JavaScriptObject create(JavaScriptObject data) /*-{
+    native JavaScriptObject create(JavaScriptObject data) /*-{
         return new $wnd.Ext.data.MemoryProxy(data);
     }-*/;
 }

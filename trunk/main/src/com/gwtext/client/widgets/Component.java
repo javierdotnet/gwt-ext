469a32bb36914ef9163dfc92453653ef8ab7bd56
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-05 12:05:27 +0000

    updated javadocs

--- trunk/main/src/com/gwtext/client/widgets/Component.java
+++ trunk/main/src/com/gwtext/client/widgets/Component.java
@@ -25,6 +25,11 @@ import com.google.gwt.user.client.Element;
 import com.gwtext.client.widgets.event.ComponentListener;
 
 //should be abstract but since ComponentMgr requires to return Component, making it concrete
+/**
+ * Base class for all major Ext components. All subclasses of Component can automatically participate in
+ * the standard Ext component lifecycle of creation, rendering and destruction. They also have automatic support for basic
+ * hide/show and enable/disable behavior.
+ */
 public class Component extends BaseExtWidget {
 
     protected Component() {
@@ -38,66 +43,121 @@ public class Component extends BaseExtWidget {
         return new Component(jsObj);
     }
 
+    /**
+     * Disable this component.
+     */
     public native void disable() /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         component.disable();
     }-*/;
 
+    /**
+     * Enable this component.
+     */
     public native void enable() /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         component.enable();
     }-*/;
 
+    /**
+     * Try to focus this component.
+     * 
+     * @param selectText True to also select the text in this component (if applicable)
+     */
     public native void focus(boolean selectText) /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         component.focus(selectText);
     }-*/;
 
+    /**
+     * Returns the id of this component.
+     * 
+     * @return the component ID
+     */
     public native String getId() /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         return component.getId();
     }-*/;
 
+    /**
+     * Hide this component.
+     */
     public native void hide() /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         component.hide();
     }-*/;
 
+    /**
+     * Returns true if this component is visible.
+     * 
+     * @return true if visible
+     */
     public native boolean isVisible() /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         return component.isVisible();
     }-*/;
 
+    /**
+     * Convenience function to hide or show this component by boolean.
+     *
+     * @param visible True to show, false to hide
+     */
     public native void setVisible(boolean visible) /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         component.setVisible(visible);
     }-*/;
 
+    /**
+     * 
+     * @return true if the component is hidden
+     */
     public native boolean isHidden() /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         return field.hidden;
     }-*/;
 
+    /**
+     * Convenience function for setting disabled/enabled by boolean.
+     * 
+     * @param disabled true to disable
+     */
     public native void setDisabled(boolean disabled) /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         component.setDisabled(disabled);
     }-*/;
 
+    /**
+     *
+     * @return true if disabled
+     */
     public native boolean isDisabled() /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         return component.disabled;
     }-*/;
 
+    /**
+     * Sets teh components disabled class.
+     *
+     * @param disabledClass the CSS class
+     */
     public native void setDisabledClass(String disabledClass) /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         component.disabledClass = disabledClass;
     }-*/;
 
+    /**
+     * Return the components disabled class.
+     *
+     * @return the disabled class
+     */
     public native String getDisabledClass() /*-{
         var field = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         return field.disabledClass();
     }-*/;
 
+    /**
+     * Show this component.
+     */
     public native void show() /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         component.show();
@@ -111,21 +171,40 @@ public class Component extends BaseExtWidget {
         component.render();
     }-*/;
 
+    /**
+     * If this is a lazy rendering component, render it to its container element.
+     *
+     * @param id the element id
+     */
     public native void render(String id) /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         component.render(id);
     }-*/;
 
+    /**
+     * If this is a lazy rendering component, render it to its container element.
+     *
+     * @param element the element
+     */
     public native void render(Element element) /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         component.render(element);
     }-*/;
 
+    /**
+     * 
+     * @return true if component has been rendered
+     */
     public native boolean isRendered() /*-{
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         return component.rendered;
     }-*/;
 
+    /**
+     * Add a component listner.
+     *
+     * @param listener the listener
+     */
     public native void addComponentListener(ComponentListener listener) /*-{
         var componentJ = this;
         var component = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;

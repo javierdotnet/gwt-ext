716c683a5b3fc9d46db1bb38e6e0d6fd0c32353c
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-10 16:30:40 +0000

    updated javadocs

--- main/src/com/gwtext/client/widgets/QuickTipsConfig.java
+++ main/src/com/gwtext/client/widgets/QuickTipsConfig.java
@@ -23,6 +23,9 @@ package com.gwtext.client.widgets;
 import com.gwtext.client.core.BaseConfig;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 
+/**
+ * Configuration class for {@link com.gwtext.client.widgets.QuickTips}.
+ */
 public class QuickTipsConfig extends BaseConfig {
 
     //todo check ext source code
@@ -36,47 +39,102 @@ public class QuickTipsConfig extends BaseConfig {
     }
     */
 
+    /**
+     * Title text to display (defaults to ''). This can be any valid HTML markup.
+     * 
+     * @param title the title
+     */
     public void setTitle(String title) {
         JavaScriptObjectHelper.setAttribute(jsObj, "title", title);
     }
 
+    /**
+     * Body text to display (defaults to ''). This can be any valid HTML markup.
+     *
+     * @param text the text
+     */
     public void setText(String text) {
         JavaScriptObjectHelper.setAttribute(jsObj, "text", text);
     }
 
-
+    /**
+     * True to turn on fade animation. Defaults to false (ClearType/scrollbar flicker issues in IE7).
+     *
+     * @param animate true to animate
+     */
     public void setAnimate(boolean animate) {
         JavaScriptObjectHelper.setAttribute(jsObj, "animate", animate);
     }
 
+    /**
+     * True to automatically hide the quick tip after the mouse exits the target element (defaults to true).
+     * Used in conjunction with hideDelay.
+     * 
+     * @param autoHide true to auto hide
+     */
     public void setAutoHide(boolean autoHide) {
         JavaScriptObjectHelper.setAttribute(jsObj, "autoHide", autoHide);
     }
 
+    /**
+     * Delay in milliseconds before the quick tip hides when autoHide = true (defaults to 200).
+     * 
+     * @param hideDelay hide delay in milliseconds
+     */
     public void setHideDelay(int hideDelay) {
         JavaScriptObjectHelper.setAttribute(jsObj, "hideDelay", hideDelay);
     }
 
+    /**
+     * True to hide the quick tip if the user clicks anywhere in the document (defaults to true).
+     * 
+     * @param hideOnClick true to hide on click
+     */
     public void setHideOnClick(boolean hideOnClick) {
         JavaScriptObjectHelper.setAttribute(jsObj, "hideOnClick", hideOnClick);
     }
 
+    /**
+     * True to automatically use the element's DOM title value if available (defaults to false).
+     *
+     * @param interceptTitles true to automatically use the element's DOM title value if available
+     */
     public void setInterceptTitles(boolean interceptTitles) {
         JavaScriptObjectHelper.setAttribute(jsObj, "interceptTitles", interceptTitles);
     }
 
+    /**
+     * The maximum width of the quick tip (defaults to 300).
+     *
+     * @param maxWidth the max width
+     */
     public void setMaxWidth(int maxWidth) {
         JavaScriptObjectHelper.setAttribute(jsObj, "maxWidth", maxWidth);
     }
 
+    /**
+     * The minimum width of the quick tip (defaults to 40).
+     *
+     * @param minWidth the min width
+     */
     public void setMinWidth(int minWidth) {
         JavaScriptObjectHelper.setAttribute(jsObj, "minWidth", minWidth);
     }
 
+    /**
+     * Delay in milliseconds before the quick tip displays after the mouse enters the target element (defaults to 500).
+     *
+     * @param showDelay the show delay
+     */
     public void setShowDelay(int showDelay) {
         JavaScriptObjectHelper.setAttribute(jsObj, "showDelay", showDelay);
     }
 
+    /**
+     * True to have the quick tip follow the mouse as it moves over the target element (defaults to false).
+     *
+     * @param trackMouse true to track mouse
+     */
     public void setTrackMouse(boolean trackMouse) {
         JavaScriptObjectHelper.setAttribute(jsObj, "trackMouse", trackMouse);
     }

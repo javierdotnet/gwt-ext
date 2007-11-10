7e63f476fbc9be553dee1ee1a90d6829fa35ac8d
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-10 17:29:52 +0000

    updated javadocs

--- main/src/com/gwtext/client/widgets/Toolbar.java
+++ main/src/com/gwtext/client/widgets/Toolbar.java
@@ -25,15 +25,28 @@ import com.google.gwt.user.client.Element;
 import com.gwtext.client.core.ExtElement;
 import com.gwtext.client.widgets.form.Field;
 
+/**
+ * Basic Toolbar class.
+ */
 public class Toolbar extends RequiredElementWidget {
 
     protected Toolbar() {
     }
 
+    /**
+     * Create a new Toolbar.
+     *
+     * @param id the toolbar ID
+     */
     public Toolbar(String id) {
         super(id);
     }
 
+    /**
+     * Create a new Toolbar.
+     *
+     * @param container the toolbar container
+     */
     public Toolbar(ExtElement container) {
         jsObj = create(container);
     }
@@ -51,6 +64,11 @@ public class Toolbar extends RequiredElementWidget {
         return new $wnd.Ext.Toolbar(el);
     }-*/;
 
+    /**
+     * Adds a toolbar button.
+     *
+     * @param button the button to add
+     */
     public void addButton(ToolbarButton button) {
         JavaScriptObject addedButtonJS = addButton(jsObj, button.getJsObj(), button.getId());
         button.registerEventHandlers();
@@ -58,6 +76,11 @@ public class Toolbar extends RequiredElementWidget {
         button.setRendered(true);
     }
 
+    /**
+     * Adds a toolbar menu button.
+     *
+     * @param button the button to add
+     */
     public void addButton(ToolbarMenuButton button) {
         JavaScriptObject addedButtonJS = addButton(jsObj, button.getJsObj(), button.getId());
         button.registerEventHandlers();
@@ -76,11 +99,21 @@ public class Toolbar extends RequiredElementWidget {
         return btn;
     }-*/;
 
+    /**
+     * Adds any standard HTML element to the toolbar.
+     * 
+     * @param html the html to add
+     */
     public native void addElement(String html) /*-{
         var toolbar = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         toolbar.addElement(html);
     }-*/;
 
+    /**
+     * Adds a dynamically rendered {@link Field} (TextField, ComboBox, etc). Note: the field should not have been rendered yet.
+     *
+     * @param field the field to add
+     */
     public void addField(Field field) {
         addField(jsObj, field.getJsObj());
     }
@@ -90,34 +123,63 @@ public class Toolbar extends RequiredElementWidget {
     }-*/;
 
     //http://extjs.com/forum/showthread.php?t=2470
+    /**
+     * Adds a fill element that forces subsequent additions to the right side of the toolbar.
+     */
     public native void addFill() /*-{
         var toolbar = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         toolbar.addFill();
     }-*/;
 
+    /**
+     * Adds any {@link ToolbarItem} or subclass.
+     *
+     * @param item the item to add
+     */
     public native void addItem(ToolbarItem item) /*-{
         var toolbar = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         var itemJS = item.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         toolbar.addItem(itemJS);
     }-*/;
 
+    /**
+     * Adds a separator.
+     *
+     * @return the separator element
+     */
     public native Element addSeparator() /*-{
         var toolbar = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         var separator = toolbar.addSeparator();
         return separator.getEl();
     }-*/;
 
+    /**
+     * Adds a spacer element.
+     * 
+     * @return the spacer element
+     */
     public native Element addSpacer() /*-{
         var toolbar = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         var spacer = toolbar.addSpacer();
         return spacer.getEl();
     }-*/;
 
+    /**
+     * Adds text to the toolbar.
+     * 
+     * @param text the text
+     */
     public native void addText(String text) /*-{
         var toolbar = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         toolbar.addText(text);
     }-*/;
 
+    /**
+     * Inserts a ToolbarItem at the specified index.
+     *
+     * @param index the index to insert at
+     * @param item the idnem to inser
+     */
     public void insertItem(int index, ToolbarItem item) {
         insertButton(jsObj, index, item.getJsObj());
     }
@@ -126,6 +188,12 @@ public class Toolbar extends RequiredElementWidget {
         toolbar.insertButton(index, item);
     }-*/;
 
+    /**
+     * Inserts a ToolbarButton at the specified index.
+     *
+     * @param index the index to insert at
+     * @param button the button to isnert
+     */
     public void insertButton(int index, ToolbarButton button) {
         insertButton(jsObj, index, button.getJsObj());
     }

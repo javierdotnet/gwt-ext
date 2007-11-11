d1d92149f42388dabc01473ed26353cc637ce6cd
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-11 16:07:22 +0000

    updated javadocs

--- main/src/com/gwtext/client/widgets/ViewConfig.java
+++ main/src/com/gwtext/client/widgets/ViewConfig.java
@@ -23,20 +23,43 @@ import com.gwtext.client.core.BaseConfig;
 import com.gwtext.client.data.Store;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 
+/**
+ * Configuration class for {@link View}.
+ */
 public class ViewConfig extends BaseConfig {
 
+    /**
+     * True for single select.
+     *
+     * @param singleSelect true for single select
+     */
     public void setSingleSelect(boolean singleSelect) {
         JavaScriptObjectHelper.setAttribute(jsObj, "singleSelect", singleSelect);
     }
 
+    /**
+     * True for multi select.
+     *
+     * @param multiSelect true for multi select.
+     */
     public void setMultiSelect(boolean multiSelect) {
         JavaScriptObjectHelper.setAttribute(jsObj, "multiSelect", multiSelect);
     }
 
+    /**
+     * The css class to add to selected nodes.
+     *
+     * @param selectedClass the CSS class
+     */
     public void setSelectedClass(String selectedClass) {
         JavaScriptObjectHelper.setAttribute(jsObj, "selectedClass", selectedClass);
     }
 
+    /**
+     * Sets the data store this view uses and refresh the view.
+     * 
+     * @param store the store
+     */
     public void setStore(Store store) {
         JavaScriptObjectHelper.setAttribute(jsObj, "store", store.getJsObj());
     }

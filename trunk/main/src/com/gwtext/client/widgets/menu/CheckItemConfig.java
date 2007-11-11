2d7f26906edc37ea9c5a97e85b8eb26dc042ad39
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-11 16:19:46 +0000

    updated javadocs

--- trunk/main/src/com/gwtext/client/widgets/menu/CheckItemConfig.java
+++ trunk/main/src/com/gwtext/client/widgets/menu/CheckItemConfig.java
@@ -23,34 +23,72 @@ package com.gwtext.client.widgets.menu;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 import com.gwtext.client.widgets.menu.event.CheckItemListener;
 
+/**
+ * Configuration class for {@link CheckItem}.
+ */
 public class CheckItemConfig extends ItemConfig {
 
     private CheckItemListener listener;
 
+    /**
+     * The text of the check item.
+     * 
+     * @param text the text
+     */
     public void setText(String text) {
         JavaScriptObjectHelper.setAttribute(jsObj, "text", text);
     }
 
+    /**
+     * True to initialize this checkbox as checked (defaults to false). Note that if this checkbox is part of a radio group (group = true) only the last item in the group that is initialized with checked = true will be rendered as checked.
+     *
+     * @param checked true to set as checked
+     */
     public void setChecked(boolean checked) {
         JavaScriptObjectHelper.setAttribute(jsObj, "checked", checked);
     }
 
+    /**
+     * All check items with the same group name will automatically be grouped into a single-select radio button group (defaults to '').
+     *
+     * @param group the group name
+     */
     public void setGroup(String group) {
         JavaScriptObjectHelper.setAttribute(jsObj, "group", group);
     }
 
+    /**
+     * The default CSS class to use for radio group check items (defaults to "x-menu-group-item").
+     * 
+     * @param groupClass the group CSS class
+     */
     public void setGroupClass(String groupClass) {
         JavaScriptObjectHelper.setAttribute(jsObj, "groupClass", groupClass);
     }
 
+    /**
+     * The default CSS class to use for check items (defaults to "x-menu-item x-menu-check-item").
+     *
+     * @param itemCls
+     */
     public void setItemCls(String itemCls) {
         JavaScriptObjectHelper.setAttribute(jsObj, "itemCls", itemCls);
     }
 
+    /**
+     * Set a CheckItem listener.
+     *
+     * @param listener the listener
+     */
     public void setCheckItemListener(CheckItemListener listener) {
         this.listener = listener;
     }
 
+    /**
+     * Return the CheckItemListener.
+     *
+     * @return the listener
+     */
     public CheckItemListener getCheckItemListener() {
         return listener;
     }

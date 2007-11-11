c974cefb2777892d08353a9a995e4997b88c929d
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-11 16:14:49 +0000

    updated javadocs

--- trunk/main/src/com/gwtext/client/widgets/menu/BaseItemConfig.java
+++ trunk/main/src/com/gwtext/client/widgets/menu/BaseItemConfig.java
@@ -24,44 +24,100 @@ import com.gwtext.client.core.BaseConfig;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 import com.gwtext.client.widgets.menu.event.BaseItemListener;
 
+/**
+ * Configuration class for {@link BaseItem}.
+ */
 public class BaseItemConfig extends BaseConfig {
 
     private BaseItemListener baseItemListener;
 
-    //TODO not sure it this works
+    /**
+     * Set the ID of the element.
+     *
+     * @param id the ID
+     */
     public void setId(String id) {
         JavaScriptObjectHelper.setAttribute(jsObj, "id", id);
     }
 
+    /**
+     * Return the ID of the element.
+     *
+     * @return the element ID
+     */
+    public String getId() {
+        return JavaScriptObjectHelper.getAttribute(jsObj, "id");
+    }
+
+    /**
+     * The CSS class to use when the item becomes activated (defaults to "x-menu-item-active")
+     *
+     * @param activeClass the active CSS class
+     */
     public void setActiveClass(String activeClass) {
         JavaScriptObjectHelper.setAttribute(jsObj, "activeClass", activeClass);
     }
 
+    /**
+     * True if this item can be visually activated (defaults to false)
+     * 
+     * @param canActivate true if can be activated
+     */
     public void setCanActivate(boolean canActivate) {
         JavaScriptObjectHelper.setAttribute(jsObj, "canActivate", canActivate);
     }
 
+    /**
+     * Length of time in milliseconds to wait before hiding after a click (defaults to 100).
+     *
+     * @param hideDelay the hide delay
+     */
     public void setHideDelay(int hideDelay) {
         JavaScriptObjectHelper.setAttribute(jsObj, "hideDelay", hideDelay);
     }
 
+    /**
+     * Length of time in milliseconds to wait before showing a hidden item  after a click (defaults to 100)
+     *
+     * @param showDelay the show delay
+     */
     public void setShowDelay(int showDelay) {
         JavaScriptObjectHelper.setAttribute(jsObj, "showDelay", showDelay);
     }
 
+    /**
+     * True to hide the containing menu after this item is clicked (defaults to true).
+     * 
+     * @param hideOnClick true to hide on click
+     */
     public void setHideOnClick(boolean hideOnClick) {
         JavaScriptObjectHelper.setAttribute(jsObj, "hideOnClick", hideOnClick);
     }
 
+    /**
+     * Return the BaseItemListener.
+     *
+     * @return
+     */
     public BaseItemListener getBaseItemListener() {
         return baseItemListener;
     }
 
+    /**
+     * Set the BaseItemListener.
+     *
+     * @param baseItemListener the listener
+     */
     public void setBaseItemListener(BaseItemListener baseItemListener) {
         this.baseItemListener = baseItemListener;
     }
 
-    //TODO not sure it this works 
+    //TODO not sure it this works
+    /**
+     * Set the icon CSS class.
+     *
+     * @param iconCls the icon class
+     */
     public void setIconCls(String iconCls) {
         JavaScriptObjectHelper.setAttribute(jsObj, "iconCls", iconCls);
     }

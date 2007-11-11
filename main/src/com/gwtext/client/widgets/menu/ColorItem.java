ead26b002e77dabf9a1eaafd42353c15953d2e50
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-11 16:21:13 +0000

    updated javadocs

--- main/src/com/gwtext/client/widgets/menu/ColorItem.java
+++ main/src/com/gwtext/client/widgets/menu/ColorItem.java
@@ -23,11 +23,23 @@ package com.gwtext.client.widgets.menu;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.gwtext.client.widgets.ColorPalette;
 
+/**
+ * A menu item that wraps the {@link ColorPalette} component.
+ *
+ */
 public class ColorItem extends Item {
 
+    /**
+     * Create a new ColorItem.
+     */
     public ColorItem() {
     }
 
+    /**
+     * Create a new ColorItem.
+     *
+     * @param config the config object
+     */
     public ColorItem(ItemConfig config) {
         super(config);
     }
@@ -36,6 +48,11 @@ public class ColorItem extends Item {
         return new $wnd.Ext.menu.ColorItem(config);    
     }-*/;
 
+    /**
+     * Return the CollorPalette associated with the menu item.
+     *
+     * @return the ColorPalette
+     */
     public native ColorPalette getPalette() /*-{
         var ci = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         return @com.gwtext.client.widgets.ColorPalette::instance(Lcom/google/gwt/core/client/JavaScriptObject;)(ci.palette);

57db37025c99490afbfdcddc0a8638324ddfb985
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2007-11-26 05:58:01 +0000

    added getValue method

--- trunk/main/src/com/gwtext/client/core/SortDir.java
+++ trunk/main/src/com/gwtext/client/core/SortDir.java
@@ -24,8 +24,8 @@ package com.gwtext.client.core;
  */
 public class SortDir {
 
-    public static SortDir ASC = new SortDir("ASC");
-    public static SortDir DESC = new SortDir("DESC");
+    public static final SortDir ASC = new SortDir("ASC");
+    public static final SortDir DESC = new SortDir("DESC");
 
     private String direction;
 
@@ -35,5 +35,21 @@ public class SortDir {
 
     public String getDirection() {
         return direction;
+    }
+
+    /**
+     * Return the sort dir value based on the passed String direction.
+     *
+     * @param direction the String direction
+     * @return the corresponding SortDir
+     */
+    public static SortDir getValue(String direction) {
+        if(ASC.getDirection().equalsIgnoreCase(direction)) {
+            return ASC;
+        } else if(DESC.getDirection().equalsIgnoreCase(direction)) {
+            return DESC;
+        } else {
+            throw new IllegalArgumentException("Unrecognized direction value " + direction);
+        }
     }
 }

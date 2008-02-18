4de388a4f4be9643a161e9f5e437dbf95e655b8d
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2008-02-18 23:52:25 +0000

    support hide / show / focus etc pre render

--- trunk/main/src/com/gwtext/client/widgets/ToolbarItem.java
+++ trunk/main/src/com/gwtext/client/widgets/ToolbarItem.java
@@ -9,6 +9,8 @@
 package com.gwtext.client.widgets;
 
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
 import com.google.gwt.user.client.Element;
 
 /**
@@ -16,80 +18,164 @@ import com.google.gwt.user.client.Element;
  */
 public class ToolbarItem extends BaseExtWidget {
 
-    protected ToolbarItem() {
-    }
+	protected ToolbarItem() {
+	}
 
-    /**
-     * Creates a new Item.
-     *
-     * @param element the html element
-     */
-    public ToolbarItem(Element element) {
-        setJsObj(create(element));
-    }
+	/**
+	 * Creates a new Item.
+	 *
+	 * @param element the html element
+	 */
+	public ToolbarItem(Element element) {
+		setJsObj(create(element));
+	}
 
-    private native JavaScriptObject create(Element element) /*-{
+	private native JavaScriptObject create(Element element) /*-{
         return new $wnd.Ext.Toolbar.Item(element);
     }-*/;
 
-    public ToolbarItem(JavaScriptObject jsObj) {
-        super(jsObj);
-    }
+	public ToolbarItem(JavaScriptObject jsObj) {
+		super(jsObj);
+	}
 
-    /**
-     * Removes and destroys this item.
-     */
-    public native void destroy()/*-{
+	public native Element getElement() /*-{
+        var tbi = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
+        var el = tbi.getEl();
+		return el === undefined ? null : el;
+	}-*/;
+
+	protected native boolean isRendered() /*-{
+        var tbi = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
+        var el = tbi.getEl();
+		return tbi.td != null && tbi.td !== undefined;
+	}-*/;
+
+	/**
+	 * Removes and destroys this item.
+	 */
+	public native void destroy()/*-{
         var tbi = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         tbi.destroy();
     }-*/;
 
-    /**
-     * Disables this item.
-     */
-    public native void disable()/*-{
+	/**
+	 * Disables this item.
+	 */
+	public void disable() {
+		if (!isRendered()) {
+			DeferredCommand.addCommand(new Command() {
+				public void execute() {
+					disable();
+				}
+			});
+		} else {
+			disableRendered();
+		}
+	}
+
+	private native void disableRendered()/*-{
         var tbi = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         tbi.disable();
     }-*/;
 
-    /**
-     * Enables this item.
-     */
-    public native void enable()/*-{
+	/**
+	 * Enables this item.
+	 */
+	public void enable() {
+		if (!isRendered()) {
+			DeferredCommand.addCommand(new Command() {
+				public void execute() {
+					enable();
+				}
+			});
+		} else {
+			enableRendered();
+		}
+	}
+
+	private native void enableRendered()/*-{
         var tbi = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         tbi.enable();
     }-*/;
 
-    /**
-     * Try to focus this item.
-     */
-    public native void focus()/*-{
+	/**
+	 * Try to focus this item.
+	 */
+	public void focus() {
+		if (!isRendered()) {
+			DeferredCommand.addCommand(new Command() {
+				public void execute() {
+					focus();
+				}
+			});
+		} else {
+			focusRendered();
+		}
+	}
+
+	private native void focusRendered()/*-{
         var tbi = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         tbi.focus();
     }-*/;
 
-    /**
-     * Hides this item.
-     */
-    public native void hide()/*-{
+	public void hide() {
+		if (!isRendered()) {
+			DeferredCommand.addCommand(new Command() {
+				public void execute() {
+					hide();
+				}
+			});
+		} else {
+			hideRendered();
+		}
+	}
+
+	/**
+	 * Hides this item.
+	 */
+	public native void hideRendered()/*-{
         var tbi = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         tbi.hide();
     }-*/;
 
-    /**
-     * Shows this item.
-     */
-    public native void show()/*-{
+	/**
+	 * Shows this item.
+	 */
+	public void show() {
+		if (!isRendered()) {
+			DeferredCommand.addCommand(new Command() {
+				public void execute() {
+					show();
+				}
+			});
+		} else {
+			showRendered();
+		}
+	}
+
+	private native void showRendered()/*-{
         var tbi = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         tbi.show();
     }-*/;
 
-    /**
-     * Convenience function for boolean show/hide.
-     *
-     * @param visible true to show
-     */
-    public native void setVisible(boolean visible)/*-{
+	/**
+	 * Convenience function for boolean show/hide.
+	 *
+	 * @param visible true to show
+	 */
+	public void setVisible(final boolean visible) {
+		if (!isRendered()) {
+			DeferredCommand.addCommand(new Command() {
+				public void execute() {
+					setVisible(visible);
+				}
+			});
+		} else {
+			setVisibleRendered(visible);
+		}
+	}
+
+	private native void setVisibleRendered(boolean visible)/*-{
         var tbi = this.@com.gwtext.client.widgets.BaseExtWidget::jsObj;
         tbi.setVisible(visible);
     }-*/;

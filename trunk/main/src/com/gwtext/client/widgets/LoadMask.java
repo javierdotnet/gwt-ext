6214cd744531bc519c96d0707eebc5e19f665f2d
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2008-02-27 04:29:50 +0000

    Ext 2.0.2 updades

--- trunk/main/src/com/gwtext/client/widgets/LoadMask.java
+++ trunk/main/src/com/gwtext/client/widgets/LoadMask.java
@@ -10,6 +10,7 @@ package com.gwtext.client.widgets;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.user.client.Element;
 import com.gwtext.client.core.JsObject;
+import com.gwtext.client.data.Store;
 import com.gwtext.client.util.JavaScriptObjectHelper;
 
 /**
@@ -59,6 +60,28 @@ public class LoadMask extends JsObject {
     /**
      * Create a new LoadMask.
      *
+     * @param element the element to mask
+     * @param message the text to display in a centered loading message box
+     * @param messageCls the CSS class to apply to the loading message element (defaults to "x-mask-loading").
+     * @param removeMask  true to create a single-use mask that is automatically destroyed after loading (useful for page loads),
+     * false to persist the mask element reference for multiple uses (e.g., for paged data widgets).
+     * @param store the store to which the mask is bound. The mask is displayed when a load request is issued, and hidden on either load sucess, or load fail.
+     */
+    public LoadMask(Element element, String message, String messageCls, boolean removeMask, Store store) {
+        this.message = message;
+        this.messageCls = messageCls;
+        this.removeMask = removeMask;
+        JavaScriptObject config = JavaScriptObjectHelper.createObject();
+        JavaScriptObjectHelper.setAttribute(config, "msg", message);
+        JavaScriptObjectHelper.setAttribute(config, "msgCls", messageCls);
+        JavaScriptObjectHelper.setAttribute(config, "removeMask", removeMask);
+        JavaScriptObjectHelper.setAttribute(config, "store", store.getJsObj());
+        jsObj = create(element, config);
+    }
+
+    /**
+     * Create a new LoadMask.
+     *
      * @param id     the element ID to mask
      * @param message the text to display in a centered loading message box
      */
@@ -86,6 +109,28 @@ public class LoadMask extends JsObject {
         JavaScriptObjectHelper.setAttribute(config, "msg", message);
         JavaScriptObjectHelper.setAttribute(config, "msgCls", messageCls);
         JavaScriptObjectHelper.setAttribute(config, "removeMask", removeMask);
+        jsObj = create(id, config);
+    }
+
+    /**
+     * Create a new LoadMask.
+     *
+     * @param id     the element ID to mask
+     * @param message the text to display in a centered loading message box
+     * @param messageCls the CSS class to apply to the loading message element (defaults to "x-mask-loading").
+     * @param removeMask  true to create a single-use mask that is automatically destroyed after loading (useful for page loads),
+     * false to persist the mask element reference for multiple uses (e.g., for paged data widgets).
+     * @param store the store to which the mask is bound. The mask is displayed when a load request is issued, and hidden on either load sucess, or load fail.
+     */
+    public LoadMask(String id, String message, String messageCls, boolean removeMask, Store store) {
+        this.message = message;
+        this.messageCls = messageCls;
+        this.removeMask = removeMask;
+        JavaScriptObject config = JavaScriptObjectHelper.createObject();
+        JavaScriptObjectHelper.setAttribute(config, "msg", message);
+        JavaScriptObjectHelper.setAttribute(config, "msgCls", messageCls);
+        JavaScriptObjectHelper.setAttribute(config, "removeMask", removeMask);
+        JavaScriptObjectHelper.setAttribute(config, "store", store.getJsObj());
         jsObj = create(id, config);
     }
 

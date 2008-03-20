7cfb0a23e17bc4e359532f90b1529893a02499ec
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2008-03-20 22:56:29 +0000

    updates

---
+++ trunk/samples/showcase2/src/com/gwtext/sample/showcase2/client/tree/TreeAppearanceSample.java
@@ -0,0 +1,65 @@
+/*
+ * GWT-Ext Widget Library
+ * Copyright(c) 2007-2008, GWT-Ext.
+ * licensing@gwt-ext.com
+ * 
+ * http://www.gwt-ext.com/license
+ */
+
+package com.gwtext.sample.showcase2.client.tree;
+
+import com.gwtext.client.widgets.Panel;
+import com.gwtext.client.widgets.layout.HorizontalLayout;
+import com.gwtext.client.widgets.layout.VerticalLayout;
+import com.gwtext.client.widgets.tree.TreePanel;
+import com.gwtext.sample.showcase2.client.SampleTree;
+import com.gwtext.sample.showcase2.client.ShowcasePanel;
+
+public class TreeAppearanceSample extends ShowcasePanel {
+
+	public Panel getViewPanel() {
+		if (panel == null) {
+			panel = new Panel();
+
+			final TreePanel treePanel = new SampleTree();
+			treePanel.setTitle("Default Appearance");
+			treePanel.setWidth(190);
+			treePanel.setHeight(400);
+
+			//create source countries tree
+			final TreePanel treePanelNoLines = new SampleTree();
+			treePanelNoLines.setTitle("No Lines");
+			treePanelNoLines.setWidth(190);
+			treePanelNoLines.setHeight(400);
+			treePanelNoLines.setLines(false);
+
+			//create target vacation tree
+			final TreePanel treePanelVistaArrows = new SampleTree();
+			treePanelVistaArrows.setTitle("Vista Arrows");
+			treePanelVistaArrows.setWidth(190);
+			treePanelVistaArrows.setHeight(400);
+			treePanelVistaArrows.setUseArrows(true);
+
+			Panel horizontalPanel = new Panel();
+			horizontalPanel.setLayout(new HorizontalLayout(20));
+			horizontalPanel.add(treePanel);
+			horizontalPanel.add(treePanelNoLines);
+			horizontalPanel.add(treePanelVistaArrows);
+
+			Panel verticalPanel = new Panel();
+			verticalPanel.setLayout(new VerticalLayout(15));
+
+			verticalPanel.add(horizontalPanel);
+
+			panel.add(verticalPanel);
+		}
+		return panel;
+	}
+
+	public String getIntro() {
+		return "<p>The first TreePanel has the default appearance with dotted lines connecting the tree nodes</p>" +
+				"<p>The second TreePanel has has no lines connecting the tree nodes. This is accomplished by calling set treePanel.setLines(false);</p>" +
+				"<p>The third TreePanel has Vista Style arrows for tree nodes. This is accomplished by calling set treePanel.setUseArrows(false);</p>" +
+				"<p>You can also use your own custom CSS to style the appearance as desired.</p>";
+	}
+}

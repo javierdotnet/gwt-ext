31cf7aa13ab194eb0bd2c9598ea524f2cf8416d1
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2008-03-30 18:34:38 +0000

    updates

--- trunk/samples/showcase2/src/com/gwtext/sample/showcase2/client/grid/BasicArrayGridSample.java
+++ trunk/samples/showcase2/src/com/gwtext/sample/showcase2/client/grid/BasicArrayGridSample.java
@@ -7,7 +7,12 @@
  */
 package com.gwtext.sample.showcase2.client.grid;
 
+import com.gwtext.client.core.EventObject;
+import com.gwtext.client.widgets.Button;
 import com.gwtext.client.widgets.Panel;
+import com.gwtext.client.widgets.Toolbar;
+import com.gwtext.client.widgets.ToolbarButton;
+import com.gwtext.client.widgets.event.ButtonListenerAdapter;
 import com.gwtext.client.widgets.grid.GridPanel;
 import com.gwtext.sample.showcase2.client.SampleGrid;
 import com.gwtext.sample.showcase2.client.ShowcasePanel;
@@ -15,24 +20,34 @@ import com.gwtext.sample.showcase2.client.ShowcasePanel;
 public class BasicArrayGridSample extends ShowcasePanel {
 
 
-    public String getSourceUrl() {
-        return "source/grid/BasicArrayGridSample.java.html";
-    }
+	public String getSourceUrl() {
+		return "source/grid/BasicArrayGridSample.java.html";
+	}
 
-    public Panel getViewPanel() {
-        if (panel == null) {
-            panel = new Panel();
-            GridPanel grid = new SampleGrid();
-            grid.setHeight(350);
-            grid.setWidth(600);
-            grid.setTitle("Array Grid");
-            panel.add(grid);
-        }
-        return panel;
-    }
+	public Panel getViewPanel() {
+		if (panel == null) {
+			panel = new Panel();
+			final GridPanel grid = new SampleGrid();
+			grid.setHeight(350);
+			grid.setWidth(600);
+			grid.setTitle("Array Grid");
 
-    public String getIntro() {
-        return "<p>This is an example of a basic Grid. The data for a Grid comes from its backing Store. In this example, the Store is populated using local array data.</p>" +
-                "<p>Some simple Column Renderer's are also assigned to the 'Change' column to style the cell depending on the data</p> ";
-    }
+			Toolbar bottomToolbar = new Toolbar();
+			bottomToolbar.addFill();
+			bottomToolbar.addButton(new ToolbarButton("Clear Sort", new ButtonListenerAdapter() {
+				public void onClick(Button button, EventObject e) {
+					grid.clearSortState(true);
+				}
+			}));
+			grid.setBottomToolbar(bottomToolbar);
+
+			panel.add(grid);
+		}
+		return panel;
+	}
+
+	public String getIntro() {
+		return "<p>This is an example of a basic Grid. The data for a Grid comes from its backing Store. In this example, the Store is populated using local array data.</p>" +
+				"<p>Some simple Column Renderer's are also assigned to the 'Change' column to style the cell depending on the data</p> ";
+	}
 }

0004f5f19aa2011d5c3f277775da6e007f3f5f6b
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2008-03-30 15:30:04 +0000

    issue 291 : support for changing label text after render

--- main/src/com/gwtext/client/widgets/form/Label.java
+++ main/src/com/gwtext/client/widgets/form/Label.java
@@ -8,74 +8,82 @@
 package com.gwtext.client.widgets.form;
 
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.DOM;
+import com.gwtext.client.util.Format;
 import com.gwtext.client.widgets.BoxComponent;
 
 /**
  * A label class for Forms.
  */
 public class Label extends BoxComponent {
-    /**
-     * Create a new Label
-     */
-    public Label() {
-    }
+	/**
+	 * Create a new Label
+	 */
+	public Label() {
+	}
 
-    /**
-     * Create a new label.
-     *
-     * @param label the label as text
-     */
-    public Label(String label) {
-        setText(label);
-    }
+	/**
+	 * Create a new label.
+	 *
+	 * @param label the label as text
+	 */
+	public Label(String label) {
+		setText(label);
+	}
 
-    public Label(JavaScriptObject jsObj) {
-        super(jsObj);
-    }
+	public Label(JavaScriptObject jsObj) {
+		super(jsObj);
+	}
 
-    protected native JavaScriptObject create(JavaScriptObject jsObj) /*-{
+	protected native JavaScriptObject create(JavaScriptObject jsObj) /*-{
         return new $wnd.Ext.form.Label(jsObj);
     }-*/;
 
-    public String getXType() {
-        return "label";
-    }
+	public String getXType() {
+		return "label";
+	}
 
-    /**
-     * Set the label as text.
-     *
-     * @param text the tab index
-     * @throws IllegalStateException this property cannot be changed after the Component has been rendered
-     */
-    public void setText(String text) {
-        setAttribute("text", text, true);
-    }
+	/**
+	 * Set the label as text.
+	 *
+	 * @param text the label text
+	 */
+	public void setText(String text) {
+		if (!isRendered()) {
+			setAttribute("text", text, true);
+		} else {
+			DOM.setElementProperty(getElement(), "innerHTML", Format.htmlEncode(text));
+		}
+	}
 
-    /**
-     * Return the label text.
-     *
-     * @return the text
-     */
-    public String getText() {
-        return getAttribute("text");
-    }
+	/**
+	 * Return the label text.
+	 *
+	 * @return the text
+	 */
+	public String getText() {
+		return getAttribute("text");
+	}
 
-    /**
-     * Set the label as html.
-     *
-     * @param html the html
-     * @throws IllegalStateException this property cannot be changed after the Component has been rendered
-     */
-    public void setHtml(String html) {
-        setAttribute("html", html, true);
-    }
+	/**
+	 * Set the label as html.
+	 *
+	 * @param html the html
+	 */
+	public void setHtml(String html) {
+		if (!isRendered()) {
+			setAttribute("html", html, true);
+		} else {
+			DOM.setElementProperty(getElement(), "innerHTML", html);
+		}
+	}
 
-    /**
-     * Return the html.
-     *
-     * @return the html
-     */
-    public String getHtml() {
-        return getAttribute("html");
-    }
+	/**
+	 * Return the html.
+	 *
+	 * @return the html
+	 */
+	public String getHtml() {
+		return getAttribute("html");
+	}
 }

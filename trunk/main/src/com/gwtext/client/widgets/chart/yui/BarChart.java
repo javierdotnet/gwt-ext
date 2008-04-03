aedbd74c33782416d37f33ab967a6f276bb60b0f
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2008-04-03 10:46:50 +0000

    polishing

--- trunk/main/src/com/gwtext/client/widgets/chart/yui/BarChart.java
+++ trunk/main/src/com/gwtext/client/widgets/chart/yui/BarChart.java
@@ -9,6 +9,9 @@ package com.gwtext.client.widgets.chart.yui;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
+/**
+ * BarChart class for the YUI Charts widget.
+ */
 public class BarChart extends CartesianChart {
 
     protected native JavaScriptObject createChart(String containerID, JavaScriptObject dataStore, JavaScriptObject config)/*-{

69c7379acdfcfc9718cfeb62243b8d22ad607dbc
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2008-04-04 04:18:20 +0000

    updates

---
+++ trunk/main/src/com/gwtext/client/dd/DragSourceConfig.java
@@ -0,0 +1,41 @@
+/*
+ * GWT-Ext Widget Library
+ * Copyright(c) 2007-2008, GWT-Ext.
+ * licensing@gwt-ext.com
+ * 
+ * http://www.gwt-ext.com/license
+ */
+package com.gwtext.client.dd;
+
+import com.gwtext.client.util.JavaScriptObjectHelper;
+
+public class DragSourceConfig extends DragDropProxyConfig {
+
+    /**
+     * A named drag drop group to which this object belongs. If a group is specified,
+     * then this object will only interact with other drag drop objects in the same group (defaults to undefined).
+     *
+     * @param ddGroup the group name
+     */
+    public void setdDdGroup(String ddGroup) {
+        JavaScriptObjectHelper.setAttribute(jsObj, "ddGroup", ddGroup);
+    }
+
+    /**
+     * The CSS class returned to the drag source when drop is allowed (defaults to "x-dd-drop-ok").
+     *
+     * @param dropAllowed drop allowed CSS
+     */
+    public void setDropAllowed(String dropAllowed) {
+        JavaScriptObjectHelper.setAttribute(jsObj, "dropAllowed", dropAllowed);
+    }
+
+    /**
+     * The CSS class returned to the drag source when drop is not allowed (defaults to "x-dd-drop-nodrop").
+     *
+     * @param dropNotAllowed drop not allowed CSS
+     */
+    public void setDropNotAllowed(String dropNotAllowed) {
+        JavaScriptObjectHelper.setAttribute(jsObj, "dropNotAllowed", dropNotAllowed);
+    }
+}

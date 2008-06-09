dd6b2badc883b445b08fda6b551baf6436b96c40
sanjiv.jivan <sanjiv.jivan@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2008-06-09 23:44:05 +0000

    updates

--- samples/showcase2/src/com/gwtext/sample/showcase2/client/pagebus/PubSubSample.java
+++ samples/showcase2/src/com/gwtext/sample/showcase2/client/pagebus/PubSubSample.java
@@ -27,9 +27,13 @@ import com.gwtext.client.data.*;
 import com.gwtext.client.pagebus.PageBus;
 import com.gwtext.client.pagebus.SubscriptionCallback;
 import com.gwtext.client.widgets.Panel;
+import com.gwtext.client.widgets.Button;
 import com.gwtext.client.widgets.event.ButtonListenerAdapter;
 import com.gwtext.client.widgets.layout.FitLayout;
 import com.gwtext.client.widgets.layout.HorizontalLayout;
+import com.gwtext.client.widgets.layout.VerticalLayout;
+import com.gwtext.client.dd.DragDrop;
+import com.gwtext.client.dd.DD;
 import com.gwtext.sample.showcase2.client.SampleGrid;
 import com.gwtext.sample.showcase2.client.ShowcasePanel;
 
@@ -50,82 +54,67 @@ public class PubSubSample extends ShowcasePanel {
 			}
 	);
 
+	public String getSourceUrl() {
+		return "source/pagebus/PubSubSample.java.html";
+	}
+
 	public Panel getViewPanel() {
 		if (panel == null) {
 			panel = new Panel();
 
-			panel.setLayout(new HorizontalLayout(15));
+			panel.setLayout(new VerticalLayout(15));
 
-			panel.addButton(new com.gwtext.client.widgets.Button("Pubhish Company Record", new ButtonListenerAdapter() {
-				public void onClick(com.gwtext.client.widgets.Button button, EventObject e) {
-					Object[] data = new Object[]{"E.I. du Pont de Nemours and Company - " + (count++), new Double(40.48), new Double(0.51), new Double(1.28), "9/1 12:00am", "DD", "Manufacturing"};
+			Panel first = new Panel();
+			first.setLayout(new HorizontalLayout(15));
+
+			SubscriberGrid sGrid1 = new SubscriberGrid();
+			sGrid1.setTitle("Subscriber1");
+			sGrid1.setHeight(200);
+			sGrid1.setWidth(200);
+			sGrid1.setIconCls("subscribe-icon");
+			first.add(sGrid1);
+
+			Button button = new Button("Pubhish Company Record", new ButtonListenerAdapter() {
+				public void onClick(Button button, EventObject e) {
+					Object[] data = new Object[]{"Company - " + (count++), new Double(40.48), new Double(0.51), new Double(1.28), "9/1 12:00am", "DD", "Manufacturing"};
 					Record record = recordDef.createRecord(data);
 					PageBus.publish(PUBLISH_TOPIC, record);
 				}
-			}));
+			});
+			button.setIconCls("publish-icon");
+			first.add(button);
+			
+			panel.add(first);
 
-			SubscriberGrid sGrid1 = new SubscriberGrid();
-			Panel sub1 = new Panel();
-			sub1.setTitle("Subscriber1");
-			sub1.setDraggable(true);
-			sub1.setHeight(200);
-			sub1.setWidth(200);
-			sub1.add(sGrid1);
-			sub1.setIconCls("subscriber-icon");
-			panel.add(sub1);
-
+			Panel second = new Panel();
+			second.setLayout(new HorizontalLayout(15));
+			
 			SubscriberGrid sGrid2 = new SubscriberGrid();
-			Panel sub2 = new Panel();
-			sub2.setTitle("Subscriber2");
-			sub2.setDraggable(true);
-			sub2.setHeight(200);
-			sub2.setWidth(200);
-			sub2.add(sGrid2);
-			sub2.setIconCls("subscriber-icon");
-			sub2.setPosition(150, 150);
-			panel.add(sub2);
+			sGrid2.setTitle("Subscriber2");
+			sGrid2.setHeight(200);
+			sGrid2.setWidth(200);
+			sGrid2.setIconCls("subscribe-icon");
+			second.add(sGrid2);
 
 			SubscriberPanel sPanel = new SubscriberPanel();
+			sPanel.setTitle("Subscriber Panel");
+			sPanel.setHeight(200);
+			sPanel.setWidth(300);
+			sPanel.setAutoScroll(true);
+			sPanel.setIconCls("subscribe-icon");
+			second.add(sPanel);
 
-			Panel sub3 = new Panel("Subscriber Panel");
-			sub3.setLayout(new FitLayout());
-			sub3.setHeight(200);
-			sub3.setWidth(200);
-			sub3.setAutoScroll(true);
-			sub3.setDraggable(true);
-			sub3.add(sPanel);
-			sub3.setIconCls("subscriber-icon");
-			panel.add(sub3);
+			panel.add(second);
 
 		}
 		return panel;
 	}
 
 	public String getIntro() {
-		return "A demonstration of Publish-Subscribe messaging.";
+		return "<p>A demonstration of Publish-Subscribe messaging. Clicking the button publishes a Record object to a topic that the other subscribers receive</p>" +
+				"<p>In this sample, the subscribers are 2 separate Grids and 1 Panel. It is worth noting that any object can be published and you are not limited to publishing String types.</p>";
 	}
 }
 
-class SubscriberPanel extends Panel {
 
-	public SubscriberPanel() {
-		PageBus.subscribe(PubSubSample.PUBLISH_TOPIC, new SubscriptionCallback() {
-			public void execute(String subject, Object message) {
-				Record record = (Record) message;
-				getBody().update("Received Company update " + record.getAsString("company"));
-			}
-		});
-	}
-}
 
-class SubscriberGrid extends SampleGrid {
-
-	public SubscriberGrid() {
-		PageBus.subscribe(PubSubSample.PUBLISH_TOPIC, new SubscriptionCallback() {
-			public void execute(String subject, Object message) {
-				Record record = (Record) message;
-				store.insert(0, record);
-			}
-		});
-	}
-}

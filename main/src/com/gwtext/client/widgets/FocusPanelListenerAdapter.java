e9963379c4f82e6dac7dd748565ae1d27199608e
mlim1972 <mlim1972@7fbbe4f3-3b31-0410-ab14-a3a9b0a89c4c>
2008-08-16 16:16:29 +0000

    Issue 389.  Adding FocusPanel into GWTExt

---
+++ main/src/com/gwtext/client/widgets/FocusPanelListenerAdapter.java
@@ -0,0 +1,58 @@
+/*
+ * GWT-Ext Widget Library
+ * Copyright 2007 - 2008, GWT-Ext LLC., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+ package com.gwtext.client.widgets;
+
+import com.gwtext.client.core.EventObject;
+import com.gwtext.client.widgets.event.PanelListenerAdapter;
+
+public class FocusPanelListenerAdapter extends PanelListenerAdapter implements
+		FocusPanelListener {
+
+	public void onFocus(FocusPanel panel, EventObject e) {
+	}
+	
+	public void onBlur(FocusPanel panel, EventObject e) {
+	}
+	
+	public void onClick(FocusPanel panel, EventObject e) {
+	}
+
+	public void onDblClick(FocusPanel panel, EventObject e) {
+	}
+
+	public void onKeyDown(FocusPanel panel, EventObject e) {
+	}
+
+
+	public void onKeyPress(FocusPanel panel, EventObject e) {
+	}
+
+	public void onKeyUp(FocusPanel panel, EventObject e) {
+	}
+
+	public void onMouseDown(FocusPanel panel, EventObject e) {
+	}
+
+	public void onMouseUp(FocusPanel panel, EventObject e) {
+	}
+
+}
